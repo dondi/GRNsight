95f35b6e7530adc81ad6e7b1f9c46e2eec4a100f
Britainjs <bsouthwi@lion.lmu.edu>
2014-02-09 03:24:30 -0800

    Added a test to check the accuracy of the spreadsheet parse.

--- server/test/spreadsheet-controller-test.js
+++ server/test/spreadsheet-controller-test.js
@@ -5,7 +5,23 @@ var should = require('should'),
     url = require('../config/config').development.url,
     xlsx = require('node-xlsx');
     
-var sheet = xlsx.parse(__dirname + '/dahlquist_wt-data_21-gene-_sample-output_20140122_est_out_1.xls');
+var sheet = xlsx.parse(__dirname + '/dahlquist_wt-data_21-gene-_sample-output_20140122_est_out_1.xlsx');
+    
+var expectedSheet = {
+  worksheets: 
+    [{name: 'Sheet1',
+      data: [
+        [45, 88],
+        [32, 'er'],
+        ['hello', 4]
+      ]},
+      {name: 'Sheet2',
+       data: [
+         ['rdsdfgrs', 'gsdfgdsfg'],
+         ['sdfgdsf']
+       ]}
+    ]
+};
 
 var worksheets = [];
 
@@ -20,6 +36,20 @@ describe('Spreadsheet Controller', function () {
          res.body[0].should.equal(sheet);
          done();
        });
+     });
+     it('should parse the spreadsheet properly', function (done) {
+       var testParse = xlsx.parse(__dirname + '/test1.xlsx');
+       testParse.worksheets.length.should.equal(2);
+       testParse.worksheets[0].name.should.equal('Sheet1');
+       testParse.worksheets[1].name.should.equal('Sheet2');
+       for(var i = 0; i < testParse.worksheets.length; i++) {
+         for(var j = 0; j < testParse.worksheets[i].data.length; j++) {
+           for(var k = 0; k < testParse.worksheets[i].data[j].length; k++) {
+             testParse.worksheets[i].data[j][k].value.should.equal(expectedSheet.worksheets[i].data[j][k]);
+           };
+         };
+       };
+       done();
      });
    });
 });
