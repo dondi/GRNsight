70ecd992c0bbbfc70ed3a1cb994e2e2de3865ca8
Britainjs <bsouthwi@lion.lmu.edu>
2014-02-13 00:41:30 -0800

    Implemented the post for the spreadsheet controller.
    Currently, the response is in a form that is somewhat legible to humans
    (although it only has minimal formatting). The next step is to make the
    response optimal for D3.

--- server/controllers/spreadsheet-controller.js
+++ server/controllers/spreadsheet-controller.js
@@ -1,8 +1,12 @@
-var multiparty = require('multiparty');
+var multiparty = require('multiparty'),
+    xlsx = require('node-xlsx'),
+    util = require('util');
 
 module.exports = function (app) {
   
-  //Send the upload file form to the web client
+  /*Send the upload file form to the web client
+   *Can be implemented in a web page later
+   */
   app.get('/', function (req, res) {
     res.writeHead(200, {'content-type': 'text/html'});
     res.end(
@@ -15,6 +19,27 @@ module.exports = function (app) {
   
   //parse the incoming form data, then parse the spreadsheet. Finally, send back json.
   app.post('/upload', function (req, res) {
-  
+    //TODO: Add file validation
+    var form = new multiparty.Form(),
+        currentSheet;
+    form.parse(req, function (err, fields, files) {
+      if (err) return res.json(400, err);
+      var sheet = xlsx.parse(files.upload[0].path);
+      //For the time being, send the result in a form readable by people
+      //TODO: Optimize the result for D3
+      res.writeHead(200, {'content-type': 'text/plain'});
+      for (var i = 0; i < sheet.worksheets.length; i++) {
+        currentSheet = sheet.worksheets[i];
+        res.write(currentSheet.name + '\n\n');
+        for (var j = 0; j < currentSheet.data.length; j++) {
+          for(var k = 0; k < currentSheet.data[j].length; k++) {
+            res.write(currentSheet.data[j][k].value + ' ');
+          }
+          res.write('\n');
+        }
+        res.write('\n');
+      }
+      res.end();
+    });
   });
 }
