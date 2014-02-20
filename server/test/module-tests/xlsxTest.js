e6d6bc834b51c048f123427fdbbef65495fbc103
Britainjs <bsouthwi@lion.lmu.edu>
2014-02-19 23:39:38 -0800

    Modified a test to debug an excel parse error.

--- server/test/module-tests/xlsxTest.js
+++ server/test/module-tests/xlsxTest.js
@@ -1,5 +1,5 @@
 var xlsx = require('node-xlsx');
-var sheet = xlsx.parse(__dirname + '/test1.xlsx');
+var sheet = xlsx.parse(__dirname + '/dahlquist_wt-data_21-gene-sample-input_20140122 copy.xlsx');
 
 console.log(sheet.worksheets[0].data);
 console.log(sheet.worksheets);
