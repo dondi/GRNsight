bba759c1b3527147bc1a2d0a2b089448b6e21a55
Britainjs <bsouthwi@lion.lmu.edu>
2014-03-20 02:44:43 -0700

    Enabled the use of self-referential edges.
    Still needs visual tweaks.

--- web-client/public/js/graph.js
+++ web-client/public/js/graph.js
@@ -21,7 +21,7 @@
       {'source':4,'target':2}
     ];
     */
-  
+    
     var width = 960,
         height = 700;
   
@@ -63,7 +63,10 @@
          .links(links)
          .start();
    
-
+    link = link.data(links)
+             .enter().append("path")
+             .attr("class", "link")
+             .attr("marker-end", "url(#arrowhead)");
          
     node = node.data(nodes)
                .enter().append("g")
@@ -87,13 +90,7 @@
       .style("stroke-width", "0")
       .style("fill", "black")
       .text(function(d) {return d.name;});
-
-
-    link = link.data(links)
-             .enter().append("line")
-             .attr("class", "link")
-             .attr("marker-end", "url(#arrowhead)");
-           
+      
     $('.node').css({
       'cursor': 'move',
       'fill': 'white',
@@ -103,14 +100,59 @@
 
     $('.link').css({
       'stroke': '#000',
+      'fill': 'none',
       'stroke-width': '1.5px'
     });
 
     function tick() {
-      link.attr("x1", function(d) { return d.source.x;})
-          .attr("y1", function(d) { return d.source.y;})
-          .attr("x2", function(d) { return d.target.x;})
-          .attr("y2", function(d) { return d.target.y;});
+      /* Allows for looping edges.
+       * From http://stackoverflow.com/questions/16358905/d3-force-layout-graph-self-linking-node
+       */
+      link.attr("d", function(d) {
+        var x1 = d.source.x,
+            y1 = d.source.y,
+            x2 = d.target.x,
+            y2 = d.target.y,
+            dx = x2 - x1,
+            dy = y2 - y1,
+            dr = Math.sqrt(dx * dx + dy * dy),
+
+            // Defaults for normal edge.
+            drx = dr,
+            dry = dr,
+            xRotation = 0, // degrees
+            largeArc = 0, // 1 or 0
+            sweep = 1; // 1 or 0
+
+            // Self edge.
+            if ( x1 === x2 && y1 === y2 ) {
+              //Move the position of the loop
+              //Couldn't figure out how to derive the width of the rectangle from here,
+              //so it is being calculated again. May need to set it when the node is created.
+              x1 = d.source.x + (d.source.name.length * 20);
+              y1 = d.source.y + 15;
+              // Fiddle with this angle to get loop oriented.
+              xRotation = 45;
+
+              // Needs to be 1.
+              largeArc = 1;
+
+              // Change sweep to change orientation of loop. 
+              sweep = 0;
+
+              // Make drx and dry different to get an ellipse
+              // instead of a circle.
+              drx = 15;
+              dry = 15;
+
+              // For whatever reason the arc collapses to a point if the beginning
+              // and ending points of the arc are the same, so kludge it.
+              x2 = x1 + 1;
+              y2 = y1;
+            } 
+
+       return "M" + x1 + "," + y1 + "A" + drx + "," + dry + " " + xRotation + "," + largeArc + "," + sweep + " " + x2 + "," + y2;
+    });
 
       node.attr("transform", function(d) {return "translate(" + d.x + "," + d.y + ")";});
     }
