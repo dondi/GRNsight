6a7303642fb5a1bae6dae6ed36bb0ce291971922
Britainjs <bsouthwi@lion.lmu.edu>
2014-04-08 00:23:07 -0700

    Allowed for weighted and unweighted input networks
    The controller will now look through the worksheets and uses the
    unweighted network sheet if it can't find the weighted sheet.

--- server/controllers/spreadsheet-controller.js
+++ server/controllers/spreadsheet-controller.js
@@ -27,39 +27,48 @@ module.exports = function (app) {
       // For the time being, send the result in a form readable by people
       //TODO: Optimize the result for D3
       res.header('Access-Control-Allow-Origin', app.get('corsOrigin'));
+      //Look for the worksheet containing the network data
       for (var i = 0; i < sheet.worksheets.length; i++) {
-        currentSheet = sheet.worksheets[i];
-        if (currentSheet.name == "network_optimized_weights") {
-          for (var j = 1; j < currentSheet.data.length; j++) {
-            try {
-              currentGene = {name: currentSheet.data[0][j].value}
-              network.genes.push(currentGene);
-            } catch (err) {
-              network.errors.push(err);
-            }
-            for(var k = 1; k < currentSheet.data[j].length; k++) {
-              try {
-                if (currentSheet.data[j][k].value != 0) {
-                  currentLink = {source: k - 1, target: j - 1, value: currentSheet.data[j][k].value};
-                  if (currentLink.value > 0) {
-                    currentLink.type = "arrowhead";
-                    currentLink.stroke = "MediumVioletRed";
-                    network.positiveWeights.push(currentLink.value);
-                  } else {
-                    currentLink.type = "repressor";
-                    currentLink.stroke = "DarkTurquoise";
-                    network.negativeWeights.push(currentLink.value);
-                  }
-                  network.links.push(currentLink);
-                }
-              } catch (err) {
-                network.errors.push(err);
+        if (sheet.worksheets[i].name == "network") {
+          //Here we have found a sheet containing simple data. We keep looking
+          //in case there is also a sheet with optimized weights
+          currentSheet = sheet.worksheets[i];
+        } else if (sheet.worksheets[i].name == "network_optimized_weights") {
+          //We found a sheet with optimized weights, which is the ideal data source.
+          //So we stop looking.
+          currentSheet = sheet.worksheets[i];
+          break;
+        }
+      }
+      
+      for (var j = 1; j < currentSheet.data.length; j++) {
+        try {
+          currentGene = {name: currentSheet.data[0][j].value}
+          network.genes.push(currentGene);
+        } catch (err) {
+          network.errors.push(err);
+        }
+        for(var k = 1; k < currentSheet.data[j].length; k++) {
+          try {
+            if (currentSheet.data[j][k].value != 0) {
+              currentLink = {source: k - 1, target: j - 1, value: currentSheet.data[j][k].value};
+              if (currentLink.value > 0) {
+                currentLink.type = "arrowhead";
+                currentLink.stroke = "MediumVioletRed";
+                network.positiveWeights.push(currentLink.value);
+              } else {
+                currentLink.type = "repressor";
+                currentLink.stroke = "DarkTurquoise";
+                network.negativeWeights.push(currentLink.value);
               }
+              network.links.push(currentLink);
             }
+          } catch (err) {
+            network.errors.push(err);
           }
         }
       }
-      res.json(network);
+      return res.json(network);
     });
   });
 }
