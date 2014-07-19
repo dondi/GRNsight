cd646860a4e22ec6d250c954d92727ce622d4ab7
dondi <dondi@lmu.edu>
2014-07-18 19:32:29 -0700

    Initial refactor toward being able to support pre-set demo files.

--- web-client/public/js/upload.js
+++ web-client/public/js/upload.js
@@ -3,54 +3,68 @@ $(function () {
   // Style of the tooltips
   $('.info').tooltip({
     placement: 'top',
-    delay: { show: 700, hide: 100}
-  })
+    delay: { show: 700, hide: 100 }
+  });
 
-  $('#upload').on('change', function(e){
+  var loadGrn = function (url, name, formData) {
+      // The presence of formData is taken to indicate a POST.
+      (formData ?
+        $.ajax({
+          url: $("#service-root").val() + "/upload",
+          data: formData,
+          processData: false,
+          contentType: false,
+          type: 'POST',
+          crossDomain: true
+        }) :
+        $.getJSON(url)
+      ).done(function (network) {
+        console.log(network);
+        $('#fileName').text(name);
+        drawGraph(0, network.genes, network.links, network.positiveWeights, network.negativeWeights, {
+          linkSlider: "#linkDistInput",
+          chargeSlider: "#chargeInput",
+          chargeDistSlider: "#chargeDistInput",
+          gravitySlider: "#gravityInput",
+          lockSliderCheckbox: "#lockSliders",
+          resetSliderButton: "#resetSliders",
+          undoResetButton: "#undoReset"
+        });
+      }).error(function (xhr, status, error) {
+        var err = JSON.parse(xhr.responseText);
+        $("#error").html(err);
+        $("#myModal").modal('show');
+      });
+    };
+
+  $('#upload').on('change', function (e) {
 
     // In google chrome, the value returned from the file input will be C:\fakepath\filename. This while loop
     // will remove the C:\fakepath\ so that it only displays the file name in the navigation bar.
-    var fullFilePath = $('input[type=file]').val()
-    var fakePathCheck = fullFilePath.search("\\\\") + 1;
-    while( fakePathCheck != 0 ) {
+    var fullFilePath = $('input[type=file]').val(),
+        fakePathCheck = fullFilePath.search("\\\\") + 1;
+
+    while (fakePathCheck != 0) {
       fullFilePath = fullFilePath.substring(fakePathCheck)
       fakePathCheck = fullFilePath.search("\\\\") + 1;
     }
+
     var formData = new FormData();
     formData.append('file', $('#upload')[0].files[0]);
-    $.ajax({
-        url: $("#service-root").val() + "/upload",
-      data: formData,
-      processData: false,
-      contentType: false,
-      type: 'POST',
-      crossDomain: true,
-      success: function (data) {
-        console.log(data);
-      }
-    }).done(function (network) {
-      $('#fileName').html( fullFilePath );
-      drawGraph(0, network.genes, network.links, network.positiveWeights, network.negativeWeights, {
-        linkSlider: "#linkDistInput",
-        chargeSlider: "#chargeInput",
-        chargeDistSlider: "#chargeDistInput",
-        gravitySlider: "#gravityInput",
-        lockSliderCheckbox: "#lockSliders",
-        resetSliderButton: "#resetSliders",
-        undoResetButton: "#undoReset"
-      });
-    }).error( function(xhr, status, error) {
-      var err = JSON.parse(xhr.responseText);
-      $( "#error" ).html(err);
-      $( "#myModal" ).modal('show');
-    });
+    loadGrn($("#service-root").val() + "/upload", fullFilePath, formData);
     e.preventDefault();
   });
 
   $('#reload').on('click', function() {
     $('#upload').trigger('change');
-  })
+  });
 
+  $('unweighted').click(function (event) {
+  });
+
+
+  $('weighted').click(function (event) {
+  });
 });
 
 /*
