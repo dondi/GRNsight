db194e6de4070b197b885bc234b20170403799b8
NAnguiano <nanguia1@lion.lmu.edu>
2014-07-30 11:54:01 -0700

    Added the collapsable sections to the documentation page. In order to do this, I had to overwrite some of the bootstrap code via css.

--- js/main.js
+++ js/main.js
@@ -3,6 +3,16 @@ $( window ).load( function() {
 	// Attach sticky functionality to the navbar
 	$( "#stickyBar" ).sticky( { topSpacing:0 } );
 
+    // Documentation page only: Makes links open their respective sections when clicked
+    $("#showSection2").click(function (){
+        $("#section1Content").collapse('hide');
+        $("#section2Content").collapse('show');
+    });
+    $("#showSection3").click(function() {
+        $("#section1Content").collapse('hide');
+        $("#section3Content").collapse('show');
+    })
+
     $( "#modified").html( document.lastModified );
 
     // Enable logo change on-hover
