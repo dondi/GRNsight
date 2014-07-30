5060566cfce1e3a76c37d3cec3ca9e40e34ee5e2
NAnguiano <nanguia1@lion.lmu.edu>
2014-07-30 08:02:01 -0700

    Added funding text to the about page and commented the js code.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
 $( window ).load( function() {
 
+	// Attach sticky functionality to the navbar
 	$( "#stickyBar" ).sticky( { topSpacing:0 } );
+
     $( "#modified").html( document.lastModified );
+
+    // Enable logo change on-hover
     $( "#GRNsightLogo" ).on( "mouseenter", function() {
     	$("#GRNsightLogo").attr("src", "images/GRNsight_logo_20140710_rollover_resized.jpg")
     })
