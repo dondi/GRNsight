400b4af164c911505ae90b01ad193f46026881e7
NAnguiano <nanguia1@lion.lmu.edu>
2014-10-17 19:53:47 -0700

    Cleaned up the html. Moved all scripts into the body and made the usage of apostropes or double quotes for tags consistent.

--- js/main.js
+++ js/main.js
@@ -3,11 +3,11 @@ $(function() {
     $(window).scroll(function() {
         // This prevents the side bar from staying stuck when the user has scrolled left. Also, when the user would zoom
         // in, the sidebar would stay stuck. This prevents that.
-        if( ($(window).scrollLeft() >= 30 && $(window).scrollTop() > 140) || ($(window).scrollTop() < 140 &&  $("#stickyBar").attr('class') != 'affix-top') ) {
-            $("#stickyBar").attr('class', 'affix-top');
+        if( ($(window).scrollLeft() >= 30 && $(window).scrollTop() > 140) || ($(window).scrollTop() < 140 &&  $('#stickyBar').attr('class') != 'affix-top') ) {
+            $('#stickyBar').attr('class', 'affix-top');
         // Re-sticks the sidebar after being unstuck due to left scroll.
-        } else if($(window).scrollTop() > 140 && $("#stickyBar").attr('class') != 'affix') {
-            $("#stickyBar").attr('class', 'affix');
+        } else if($(window).scrollTop() > 140 && $('#stickyBar').attr('class') != 'affix') {
+            $('#stickyBar').attr('class', 'affix');
         } 
     });
 
@@ -35,18 +35,18 @@ $(function() {
     });
 
     // Enable logo change on-hover
-    $( "#GRNsightLogo" ).on( "mouseenter", function() {
-    	$("#GRNsightLogo").attr("src", "images/GRNsight_logo_20140710_rollover_resized.jpg")
-    }).on("mouseleave", function() {
-    	$("#GRNsightLogo").attr("src", "images/GRNsight_logo_20140710_main_resized.jpg")
+    $( '#GRNsightLogo' ).on( 'mouseenter', function() {
+    	$('#GRNsightLogo').attr('src', 'images/GRNsight_logo_20140710_rollover_resized.jpg')
+    }).on('mouseleave', function() {
+    	$('#GRNsightLogo').attr('src', 'images/GRNsight_logo_20140710_main_resized.jpg')
     });
 
     // Open a hashed element if it is collapsed.  This is a little trickier than it sounds,
     // because then we also have to open any parents that are also collapsed.
     if (location.hash) {
         var $elementToOpen = $(location.hash);
-        if ($elementToOpen.is(".collapse:not(.in)")) {
-            $elementToOpen.parents(".collapse:not(.in)").collapse('show');
+        if ($elementToOpen.is('.collapse:not(.in)')) {
+            $elementToOpen.parents('.collapse:not(.in)').collapse('show');
             $elementToOpen.collapse('show');
         }
     }
