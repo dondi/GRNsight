c88552d8e9896ce245af6abc9d883637a53da534
dondi <dondi@lmu.edu>
2015-02-16 23:58:00 -0800

    Implement path parameter.

--- server/controllers/ga-controller.js
+++ server/controllers/ga-controller.js
@@ -1,6 +1,8 @@
 var google = require('googleapis'),
     ga = google.analytics('v3')
 
+var moment = require('moment')
+
 module.exports = function (app) {
   app.get('/ga', function (req, res) {
     var jwt = new google.auth.JWT(
@@ -24,8 +26,9 @@ module.exports = function (app) {
         auth: jwt,
         ids: 'ga:91279024',
         'start-date': '2014-01-01',
-        'end-date': '2015-02-09',
-        'metrics': 'ga:sessions',
+        'end-date': moment().format('YYYY-MM-DD'),
+        'metrics': 'ga:pageviews',
+        'filters': 'ga:pagePath==/GRNsight/' + (req.query.path || '')
       }, function (err, result) {
         if (err) {
           console.log('api error', err, result)
