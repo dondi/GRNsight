aaa64c1ca2d651ca2a28960d0fdb85d7c269822b
dondi <dondi@lmu.edu>
2015-02-18 09:47:41 -0800

    Initial integration of Google Analytics reporting script.

---
+++ js/ga-report.js
@@ -0,0 +1,5 @@
+$(function () {
+    $.getJSON("http://grnsight.cs.lmu.edu/server/ga?path=" + location.pathname.split("/").pop(), function (result) {
+        $(".ga-report").text(result);
+    });
+});
