9d775a44f7fb3edf770a837c69cecc84f971a001
dondi <dondi@lmu.edu>
2015-02-24 14:02:06 -0800

    Soooooooo close.

--- test/test.js
+++ test/test.js
@@ -1,13 +1,20 @@
-var assert = require('chai').assert;
+var assert = require('chai').assert,
+    xlsx = require('node-xlsx');
+
 var spreadsheetController = require(__dirname + '/../server/controllers' + '/spreadsheet-controller')();
 
-describe('Gene Name Modifications', function(){
-  describe('duplicate-gene-top-output', function(){
-    it('should return 1 duplicate gene error', function(){
-      var sheet = {genes: [21], errors: [1]},
-        possibleCause;
-      sheet.errors[0].possibleCause("There exists a duplicate for source gene ACE2.")
-      assert.equal("There exists a duplicate for source gene ACE2.", spreadsheetController.parseSheet(sheet).possibleCause);
+describe('Gene Name Modifications', function () {
+  describe('duplicate-gene-top-output', function () {
+    it('should return 1 duplicate gene error', function () {
+      var sheet = xlsx.parse('test-files/gene-name-modifications/duplicate-gene-top-output.xlsx');
+      console.log(sheet);
+      var network = spreadsheetController.parseSheet(sheet);
+
+      assert.equal(1, network.errors.length);
+      assert.equal(
+        "There exists a duplicate for source gene ACE2.",
+        network.errors[0].possibleCause
+      );
     })
   })
 })
