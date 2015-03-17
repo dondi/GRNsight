9d6317d62f3709f76b92658f202a92691ba3e04b
NAnguiano <nanguia1@lion.lmu.edu>
2015-03-17 10:11:19 -0700

    Testing google analytics tweaks.

--- js/ga-report.js
+++ js/ga-report.js
@@ -1,5 +1,6 @@
 $(function () {
     $.getJSON("http://grnsight.cs.lmu.edu/server/ga?path=" + location.pathname.split("/").pop(), function (result) {
         $(".ga-report").text(result);
+        console.log(result);
     });
 });
