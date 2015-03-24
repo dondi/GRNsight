1ad2d20c4d9774bfe3c51d080b7f62c4a2c36fd3
Anu <anu.varshneya@gmail.com>
2015-03-24 15:09:54 -0700

    consolidated test functions for each test code

--- test/test.js
+++ test/test.js
@@ -12,103 +12,83 @@ describe('gene-name-modifications', function () {
       assert.equal(0, network.errors.length);
   }
 
-  function duplicateGene1(input) {  
-        var sheet = xlsx.parse(input),
-            network = spreadsheetController.parseSheet(sheet);
-
-        assert.equal(1, network.errors.length);
-        assert.equal(
-          "DUPLICATE_GENE",
-          network.errors[0].errorCode
-        );
-  }
-
-  function duplicateGene2(input) {  
-        var sheet = xlsx.parse(input),
-            network = spreadsheetController.parseSheet(sheet);
-
-        assert.equal(2, network.errors.length);
-        assert.equal(
-          "DUPLICATE_GENE",
-          network.errors[0].errorCode
-        );
-        assert.equal(
-          "DUPLICATE_GENE",
-          network.errors[1].errorCode
-        );
-  }
-
-  function invalidGeneLength1(input){
+  function duplicateGene(input, frequency) {  
     var sheet = xlsx.parse(input),
-          network = spreadsheetController.parseSheet(sheet);
+        network = spreadsheetController.parseSheet(sheet);
 
-      assert.equal(1, network.errors.length);
+    assert.equal(frequency, network.errors.length);
+
+    for(var i = 0; i < frequency; i++) {
+      assert.equal(
+        "DUPLICATE_GENE",
+        network.errors[i].errorCode
+      );
+    }      
+  }
+
+  function invalidGeneLength(input, frequency){
+    var sheet = xlsx.parse(input),
+        network = spreadsheetController.parseSheet(sheet);
+
+    assert.equal(frequency, network.errors.length);
+      
+    for(var i = 0; i < frequency; i++) {
       assert.equal(
         "INVALID_GENE_LENGTH",
         network.errors[0].errorCode
       );
+    }
   }
   
-  function invalidGeneLength2(input){
+
+  function corruptGene(input, frequency) {
     var sheet = xlsx.parse(input),
-          network = spreadsheetController.parseSheet(sheet);
+        network = spreadsheetController.parseSheet(sheet);
 
-      assert.equal(2, network.errors.length);
-      assert.equal(
-        "INVALID_GENE_LENGTH",
-        network.errors[0].errorCode
-      );
-      assert.equal(
-        "INVALID_GENE_LENGTH",
-        network.errors[1].errorCode
-      );
-  }
-
-  function corruptGene(input) {
-    var sheet = xlsx.parse(input),
-          network = spreadsheetController.parseSheet(sheet);
-
-      assert.equal(1, network.errors.length);
+    assert.equal(frequency, network.errors.length);
+      
+    for(var i = 0; i < frequency; i++) {
       assert.equal(
         "CORRUPT_GENE",
         network.errors[0].errorCode
       );
+    }
   }
 
   describe('duplicate-gene-side-and-top', function () {
       it('should return 2 duplicate gene errors', function () {
-        duplicateGene2('test-files/gene-name-modifications/duplicate-gene-side-and-top-input.xlsx');
-        duplicateGene2('test-files/gene-name-modifications/duplicate-gene-side-and-top-output.xlsx');
+        duplicateGene('test-files/gene-name-modifications/duplicate-gene-side-and-top-input.xlsx', 2);
+        duplicateGene('test-files/gene-name-modifications/duplicate-gene-side-and-top-output.xlsx', 2);
 
       })
     })
 
   describe('duplicate-gene-side', function () {
     it('should return 1 duplicate gene error', function () {
-      duplicateGene1('test-files/gene-name-modifications/duplicate-gene-side-input.xlsx');
-      duplicateGene1('test-files/gene-name-modifications/duplicate-gene-side-output.xlsx');
+      duplicateGene('test-files/gene-name-modifications/duplicate-gene-side-input.xlsx', 1);
+      duplicateGene('test-files/gene-name-modifications/duplicate-gene-side-output.xlsx', 1);
 
     })
   })
 
 describe('duplicate-gene-top', function () {
     it('should return 1 duplicate gene error', function () {
-      duplicateGene1('test-files/gene-name-modifications/duplicate-gene-top-input.xlsx');
-      duplicateGene1('test-files/gene-name-modifications/duplicate-gene-top-output.xlsx');
+      duplicateGene('test-files/gene-name-modifications/duplicate-gene-top-input.xlsx', 1);
+      duplicateGene('test-files/gene-name-modifications/duplicate-gene-top-output.xlsx', 1);
     })
   })
 
   describe('long-gene-name-related', function () {
     it('should return 1 long gene name error', function () {
-      invalidGeneLength1('test-files/gene-name-modifications/long-gene-name-related-input.xlsx');
-      invalidGeneLength1('test-files/gene-name-modifications/long-gene-name-related-output.xlsx');
+      invalidGeneLength('test-files/gene-name-modifications/long-gene-name-related-input.xlsx', 1);
+      invalidGeneLength('test-files/gene-name-modifications/long-gene-name-related-output.xlsx', 1);
     })
   })
 
   describe('long-gene-name-unrelated', function () {
-    it('should return 2 long gene name error', function () {
-      invalidGeneLength1('test-files/gene-name-modifications/long-gene-name-unrelated-input.xlsx');
-      invalidGeneLength2('test-files/gene-name-modifications/long-gene-name-unrelated-output.xlsx');
+    it('should return 1 or 2 long gene name error', function () {
+      invalidGeneLength('test-files/gene-name-modifications/long-gene-name-unrelated-input.xlsx', 1);
+      invalidGeneLength('test-files/gene-name-modifications/long-gene-name-unrelated-output.xlsx', 2);
     })
   })
 
@@ -129,22 +109,22 @@ describe('duplicate-gene-top', function () {
 
   describe('missing-gene-name-side', function () {
     it('should return 1 missing gene error', function () {
-      corruptGene('test-files/gene-name-modifications/missing-gene-name-side-input.xlsx');
-      corruptGene('test-files/gene-name-modifications/missing-gene-name-side-output.xlsx');
+      corruptGene('test-files/gene-name-modifications/missing-gene-name-side-input.xlsx', 1);
+      corruptGene('test-files/gene-name-modifications/missing-gene-name-side-output.xlsx', 1);
     })
   })
 
   describe('missing-gene-name-top-and-side', function () {
     it('should return 1 missing gene error', function () {
-      corruptGene('test-files/gene-name-modifications/missing-gene-name-top-and-side-input.xlsx');
-      corruptGene('test-files/gene-name-modifications/missing-gene-name-top-and-side-output.xlsx');
+      corruptGene('test-files/gene-name-modifications/missing-gene-name-top-and-side-input.xlsx', 1);
+      corruptGene('test-files/gene-name-modifications/missing-gene-name-top-and-side-output.xlsx', 1);
     })
   })
 
   describe('missing-gene-name-top', function () {
     it('should return 1 missing gene error', function () {
-      corruptGene('test-files/gene-name-modifications/missing-gene-name-top-input.xlsx');
-      corruptGene('test-files/gene-name-modifications/missing-gene-name-top-output.xlsx');
+      corruptGene('test-files/gene-name-modifications/missing-gene-name-top-input.xlsx', 1);
+      corruptGene('test-files/gene-name-modifications/missing-gene-name-top-output.xlsx', 1);
     })
   })
 })
