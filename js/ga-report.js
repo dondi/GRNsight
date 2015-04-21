083bf19396bab9c1a238480d4a32409f212dcf91
NAnguiano <nanguia1@lion.lmu.edu>
2015-04-20 23:21:31 -0700

    Made a quick change to the ga-report code for test.

--- js/ga-report.js
+++ js/ga-report.js
@@ -1,6 +1,6 @@
 $(function () {
     $.getJSON("http://grnsight.cs.lmu.edu/server/ga?path=" + location.pathname.split("/").pop(), function (result) {
         $(".ga-report").text(result);
-        console.log(result);
+        console.log(location.pathname.split("/"));
     });
 });
