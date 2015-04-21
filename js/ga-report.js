53d73fa1f89f420090d32dad8dba4489805f4a26
NAnguiano <nanguia1@lion.lmu.edu>
2015-04-20 23:46:28 -0700

    Found the source of the bug. Implemented functioanlity to combine index and home page total.

--- js/ga-report.js
+++ js/ga-report.js
@@ -1,6 +1,15 @@
 $(function () {
     $.getJSON("http://grnsight.cs.lmu.edu/server/ga?path=" + location.pathname.split("/").pop(), function (result) {
-        $(".ga-report").text(result);
-        console.log(result);
+    	if(location.pathname.split("/").pop() === "") {
+    		$.getJSON("http://grnsight.cs.lmu.edu/server/ga?path=index.html", function (result2) {
+    			$(".ga-report").text(result + result2);
+    		})
+    	} else if(location.pathname.split("/").pop() === "index.html") {
+    		$.getJSON("http://grnsight.cs.lmu.edu/server/ga?path=", function (result2) {
+    			$(".ga-report").text(result + result2)
+    		})
+    	} else {
+    		$(".ga-report").text(result);
+    	}
     });
 });
