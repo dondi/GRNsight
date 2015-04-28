81f74dfccd6a52e90d861f7ab8c0041d5ca4700f
NAnguiano <nanguia1@lion.lmu.edu>
2015-04-28 14:23:49 -0700

    Should be working this time.

--- js/ga-report.js
+++ js/ga-report.js
@@ -4,21 +4,21 @@ $(function () {
     		$.getJSON("http://grnsight.cs.lmu.edu/server/ga?path=index.html", function (result2) {
     			$(".ga-report").text(result + result2);
     		})
-            $.getJSON("http://grnsight.cs.lmu.edu/server/ga?path=upload"), function (uploadCount) {
+            $.getJSON("http://grnsight.cs.lmu.edu/server/ga?path=upload", function (uploadCount) {
                 $(".ga-upload").text(uploadCount);
             })
     	} else if(location.pathname.split("/").pop() === "index.html") {
     		$.getJSON("http://grnsight.cs.lmu.edu/server/ga?path=", function (result2) {
     			$(".ga-report").text(result + result2)
     		})
-            $.getJSON("http://grnsight.cs.lmu.edu/server/ga?path=upload"), function (uploadCount) {
+            $.getJSON("http://grnsight.cs.lmu.edu/server/ga?path=upload", function (uploadCount) {
                 $(".ga-upload").text(uploadCount);
             })
     	} else {
     		$(".ga-report").text(result);
     	}
         if(location.pathname.split("/").pop() === "beta.html") {
-            $.getJSON("http://grnsight.cs.lmu.edu/server/ga?path=upload"), function (uploadCount) {
+            $.getJSON("http://grnsight.cs.lmu.edu/server/ga?path=upload", function (uploadCount) {
                 $(".ga-upload").text(uploadCount);
             })
         }
