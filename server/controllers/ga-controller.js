2b39b8afbefbf6eb97a4ebdd09b1d1525cd8b2a9
dondi <dondi@lmu.edu>
2015-06-17 00:04:53 -0700

    Add Google Analytics support for GRNmap sister project (GRNmap issue 72). Functional but needs DRY refactoring.

--- server/controllers/ga-controller.js
+++ server/controllers/ga-controller.js
@@ -12,7 +12,7 @@ module.exports = function (app) {
       ['https://www.googleapis.com/auth/analytics.readonly'],
       null)
 
-    if (app.get('env') !== 'developer') {
+    if (app.get('env') !== 'development') {
       res.header('Access-Control-Allow-Origin', 'http://dondi.github.io')
     }
 
@@ -29,6 +29,41 @@ module.exports = function (app) {
         'end-date': moment().format('YYYY-MM-DD'),
         'metrics': 'ga:pageviews',
         'filters': 'ga:pagePath==/GRNsight/' + (req.query.path || '')
+      }, function (err, result) {
+        if (err) {
+          console.log('api error', err, result)
+        }
+
+        res.send(result && result.rows && result.rows[0] ? result.rows[0][0] : '(unknown)')
+      })
+    })
+  })
+
+  app.get('/grnmap', function (req, res) {
+    var jwt = new google.auth.JWT(
+      process.env.GRNMAP_SERVICE_EMAIL,
+      'grnmap.pem',
+      null,
+      ['https://www.googleapis.com/auth/analytics.readonly'],
+      null)
+
+    if (app.get('env') !== 'development') {
+      res.header('Access-Control-Allow-Origin', 'http://kdahlquist.github.io')
+    }
+
+    jwt.authorize(function (err, tokens) {
+      if (err) {
+        console.log('jwt error', err)
+        return
+      }
+
+      ga.data.ga.get({
+        auth: jwt,
+        ids: 'ga:97014398',
+        'start-date': '2014-01-01',
+        'end-date': moment().format('YYYY-MM-DD'),
+        'metrics': 'ga:pageviews',
+        'filters': 'ga:pagePath==/GRNmap/' + (req.query.path || '')
       }, function (err, result) {
         if (err) {
           console.log('api error', err, result)
