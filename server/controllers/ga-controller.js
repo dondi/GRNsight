b25be3489b4efdbef9f7b65ff6bd8394e8ad7503
dondi <dondi@lmu.edu>
2015-06-17 00:30:22 -0700

    DRY-refactor Google Analytics query code.

--- server/controllers/ga-controller.js
+++ server/controllers/ga-controller.js
@@ -4,73 +4,58 @@ var google = require('googleapis'),
 var moment = require('moment')
 
 module.exports = function (app) {
-  app.get('/ga', function (req, res) {
-    var jwt = new google.auth.JWT(
-      process.env.SERVICE_EMAIL,
-      'grnsight.pem',
-      null,
-      ['https://www.googleapis.com/auth/analytics.readonly'],
-      null)
+  var gaQuery = function (req, res, options) {
+        var jwt = new google.auth.JWT(
+          options.serviceEmail,
+          options.pemFilename,
+          null,
+          ['https://www.googleapis.com/auth/analytics.readonly'],
+          null)
 
-    if (app.get('env') !== 'development') {
-      res.header('Access-Control-Allow-Origin', 'http://dondi.github.io')
-    }
-
-    jwt.authorize(function (err, tokens) {
-      if (err) {
-        console.log('jwt error', err)
-        return
-      }
-
-      ga.data.ga.get({
-        auth: jwt,
-        ids: 'ga:91279024',
-        'start-date': '2014-01-01',
-        'end-date': moment().format('YYYY-MM-DD'),
-        'metrics': 'ga:pageviews',
-        'filters': 'ga:pagePath==/GRNsight/' + (req.query.path || '')
-      }, function (err, result) {
-        if (err) {
-          console.log('api error', err, result)
+        if (app.get('env') !== 'development') {
+          res.header('Access-Control-Allow-Origin', options.origin)
         }
 
-        res.send(result && result.rows && result.rows[0] ? result.rows[0][0] : '(unknown)')
-      })
+        jwt.authorize(function (err, tokens) {
+          if (err) {
+            console.log('jwt error', err)
+            return
+          }
+
+          ga.data.ga.get({
+            auth: jwt,
+            ids: options.accountId,
+            'start-date': '2014-01-01',
+            'end-date': moment().format('YYYY-MM-DD'),
+            'metrics': 'ga:pageviews',
+            'filters': 'ga:pagePath==/' + options.pathRoot + '/' + (req.query.path || '')
+          }, function (err, result) {
+            if (err) {
+              console.log('api error', err, result)
+            }
+
+            res.send(result && result.rows && result.rows[0] ? result.rows[0][0] : '(unknown)')
+          })
+        })
+      }
+
+  app.get('/ga', function (req, res) {
+    gaQuery(req, res, {
+      serviceEmail: process.env.SERVICE_EMAIL,
+      pemFilename: 'grnsight.pem',
+      origin: 'http://dondi.github.io',
+      accountId: 'ga:91279024',
+      pathRoot: 'GRNsight'
     })
   })
 
   app.get('/grnmap', function (req, res) {
-    var jwt = new google.auth.JWT(
-      process.env.GRNMAP_SERVICE_EMAIL,
-      'grnmap.pem',
-      null,
-      ['https://www.googleapis.com/auth/analytics.readonly'],
-      null)
-
-    if (app.get('env') !== 'development') {
-      res.header('Access-Control-Allow-Origin', 'http://kdahlquist.github.io')
-    }
-
-    jwt.authorize(function (err, tokens) {
-      if (err) {
-        console.log('jwt error', err)
-        return
-      }
-
-      ga.data.ga.get({
-        auth: jwt,
-        ids: 'ga:97014398',
-        'start-date': '2014-01-01',
-        'end-date': moment().format('YYYY-MM-DD'),
-        'metrics': 'ga:pageviews',
-        'filters': 'ga:pagePath==/GRNmap/' + (req.query.path || '')
-      }, function (err, result) {
-        if (err) {
-          console.log('api error', err, result)
-        }
-
-        res.send(result && result.rows && result.rows[0] ? result.rows[0][0] : '(unknown)')
-      })
+    gaQuery(req, res, {
+      serviceEmail: process.env.GRNMAP_SERVICE_EMAIL,
+      pemFilename: 'grnmap.pem',
+      origin: 'http://kdahlquist.github.io',
+      accountId: 'ga:97014398',
+      pathRoot: 'GRNmap'
     })
   })
 }
