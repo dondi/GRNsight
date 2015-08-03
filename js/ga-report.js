50cb0dbab7421aa3b9622681a493ce1280df0492
dondi <dondi@lmu.edu>
2015-08-02 18:32:16 -0700

    Temporary switch to grnsight2.

--- js/ga-report.js
+++ js/ga-report.js
@@ -1,24 +1,24 @@
 $(function () {
-    $.getJSON("http://grnsight.cs.lmu.edu/server/ga?path=" + location.pathname.split("/").pop(), function (result) {
+    $.getJSON("http://grnsight2.cs.lmu.edu/server/ga?path=" + location.pathname.split("/").pop(), function (result) {
     	if(location.pathname.split("/").pop() === "") {
-    		$.getJSON("http://grnsight.cs.lmu.edu/server/ga?path=index.html", function (result2) {
+    		$.getJSON("http://grnsight2.cs.lmu.edu/server/ga?path=index.html", function (result2) {
     			$(".ga-report").text(result + result2);
     		})
-            $.getJSON("http://grnsight.cs.lmu.edu/server/ga?path=upload", function (uploadCount) {
+            $.getJSON("http://grnsight2.cs.lmu.edu/server/ga?path=upload", function (uploadCount) {
                 $(".ga-upload").text(uploadCount);
             })
     	} else if(location.pathname.split("/").pop() === "index.html") {
-    		$.getJSON("http://grnsight.cs.lmu.edu/server/ga?path=", function (result2) {
+    		$.getJSON("http://grnsight2.cs.lmu.edu/server/ga?path=", function (result2) {
     			$(".ga-report").text(result + result2);
     		})
-            $.getJSON("http://grnsight.cs.lmu.edu/server/ga?path=upload", function (uploadCount) {
+            $.getJSON("http://grnsight2.cs.lmu.edu/server/ga?path=upload", function (uploadCount) {
                 $(".ga-upload").text(uploadCount);
             })
     	} else {
     		$(".ga-report").text(result);
     	}
         if(location.pathname.split("/").pop() === "beta.html") {
-            $.getJSON("http://grnsight.cs.lmu.edu/server/ga?path=upload", function (uploadCount) {
+            $.getJSON("http://grnsight2.cs.lmu.edu/server/ga?path=upload", function (uploadCount) {
                 $(".ga-upload").text(uploadCount);
             })
         }
