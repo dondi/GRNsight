5919248bcfcece29c2d19d903cbcec3b219bd11f
dondi <dondi@lmu.edu>
2015-08-08 19:42:32 -0700

    Restore grnsight-without-a-2 plus refactor for DRYness.

--- js/ga-report.js
+++ js/ga-report.js
@@ -1,26 +1,37 @@
 $(function () {
-    $.getJSON("http://grnsight2.cs.lmu.edu/server/ga?path=" + location.pathname.split("/").pop(), function (result) {
-    	if(location.pathname.split("/").pop() === "") {
-    		$.getJSON("http://grnsight2.cs.lmu.edu/server/ga?path=index.html", function (result2) {
-    			$(".ga-report").text(result + result2);
-    		})
-            $.getJSON("http://grnsight2.cs.lmu.edu/server/ga?path=upload", function (uploadCount) {
-                $(".ga-upload").text(uploadCount);
-            })
-    	} else if(location.pathname.split("/").pop() === "index.html") {
-    		$.getJSON("http://grnsight2.cs.lmu.edu/server/ga?path=", function (result2) {
-    			$(".ga-report").text(result + result2);
-    		})
-            $.getJSON("http://grnsight2.cs.lmu.edu/server/ga?path=upload", function (uploadCount) {
-                $(".ga-upload").text(uploadCount);
-            })
-    	} else {
-    		$(".ga-report").text(result);
-    	}
-        if(location.pathname.split("/").pop() === "beta.html") {
-            $.getJSON("http://grnsight2.cs.lmu.edu/server/ga?path=upload", function (uploadCount) {
-                $(".ga-upload").text(uploadCount);
-            })
+    var getCount = function (path, callback) {
+            $.getJSON("http://grnsight.cs.lmu.edu/server/ga?path=" + path).done(callback);
+        },
+
+        setReportResult = function (result) {
+            $(".ga-report").text(result);            
+        },
+
+        getUploadCount = function () {
+            getCount("upload", function (uploadCount) {
+                setReportResult(uploadCount);
+            });
+        },
+
+        pathTail = location.pathname.split("/").pop();
+
+    getCount(pathTail, function (pathResult) {
+        if (pathTail === "") {
+            getCount("index.html", function (indexCount) {
+                setReportResult(pathResult + indexResult);
+            });
+
+            getUploadCount();
+        } else if (pathTail === "index.html") {
+            getCount("", function (homeResult) {
+                setReportResult(pathResult + homeResult);
+            });
+
+            getUploadCount();
+        } else if (pathTail === "beta.html") {
+            getUploadCount();
+        } else {
+            setReportResult(pathResult);
         }
     });
 });
