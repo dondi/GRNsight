63417f684c8b99a9a7ffa427213525ab9ec44ba7
NAnguiano <nanguia1@lion.lmu.edu>
2015-09-17 15:22:40 -0700

    Initial cleaning of main code.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,53 +1,99 @@
 $(function() {
 
-    $(window).scroll(function() {
-        // This prevents the side bar from staying stuck when the user has scrolled left. Also, when the user would zoom
-        // in, the sidebar would stay stuck. This prevents that.
-        if( ($(window).scrollLeft() >= 1 && $(window).scrollTop() > 140) || ($(window).scrollTop() < 140 &&  $('#stickyBar').attr('class') != 'affix-top') ) {
-            $('#stickyBar').attr('class', 'affix-top');
-        // Re-sticks the sidebar after being unstuck due to left scroll.
-        } else if($(window).scrollTop() > 140 && $('#stickyBar').attr('class') != 'affix') {
-            $('#stickyBar').attr('class', 'affix');
+    var initializePage = function () {
+        changeImageOnHover('#GRNsightLogo', '/GRNsight/assets/images/GRNsight_logo_20140710_main_resized.jpg', '/GRNsight/assets/images/GRNsight_logo_20140710_rollover_resized.jpg');
+        configureSidebarScrollingBehavior( '#stickyBar' );
+        openCollapsedHashedElements();
+
+        var pageIsDocumentationPage = (location.pathname === "/GRNsight/documentation.html");
+        if ( pageIsDocumentationPage )  {
+            setupDocumentationPanels();
         } 
-    });
+    }
 
-    var setupClickableItems = function (clickableSelector, contentId, targetHref) {
-        // Documentation page only: Makes links open their respective sections when clicked
-        $(contentId).on('shown.bs.collapse', function () {
-            window.location.href = targetHref;
+    var changeImageOnHover = function ( contentID, imageSrcDefault, imageSrcOnHover ) {
+        $( contentID ).on( 'mouseenter', function () {
+            $( contentID ).attr('src', imageSrcOnHover)
+        }).on( 'mouseleave', function () {
+            $( contentID ).attr('src', imageSrcDefault)
+        })
+    }
+
+    var configureSidebarScrollingBehavior = function ( sidebarID ) {
+        $( window ).scroll( function() {
+            disableHorizontalMovement( sidebarID );
         });
-    
-        // If the links are already open, the above handlers won't fire, so we force the page to change in that case
-        // Using .on because using .click doesn't work for some reason
-        $(clickableSelector).on('click', function () {
-            $(contentId).collapse('show');
-            if($(contentId).attr('class') === 'panel-collapse collapse in') {
-                window.location.href = targetHref;
-            };
-        });
-    };
+    }
 
-    [
-        [ '.showSection2', '#section2Content', '#section2' ],
-        [ '.showSection3', '#section3Content', '#section3' ]
-    ].forEach(function (clickableItem) {
-        setupClickableItems.apply(null, clickableItem);
-    });
+    var leftWindowBoundary = 1,
+        sidebarScrollStartPoint = 140;
 
-    // Enable logo change on-hover
-    $( '#GRNsightLogo' ).on( 'mouseenter', function() {
-    	$('#GRNsightLogo').attr('src', '/GRNsight/assets/images/GRNsight_logo_20140710_rollover_resized.jpg')
-    }).on('mouseleave', function() {
-    	$('#GRNsightLogo').attr('src', '/GRNsight/assets/images/GRNsight_logo_20140710_main_resized.jpg')
-    });
+    var disableHorizontalMovement = function ( sidebarID ) {
+        var userHasScrolledRight = $(window).scrollLeft() >= leftWindowBoundary,
+            sidebarShouldBeScrolling = $(window).scrollTop() > sidebarScrollStartPoint,
+            sidebarIsAttached = ($( sidebarID ).attr('class')) === 'affix' ? true : false;
 
-    // Open a hashed element if it is collapsed.  This is a little trickier than it sounds,
-    // because then we also have to open any parents that are also collapsed.
-    if (location.hash) {
-        var $elementToOpen = $(location.hash);
-        if ($elementToOpen.is('.collapse:not(.in)')) {
-            $elementToOpen.parents('.collapse:not(.in)').collapse('show');
-            $elementToOpen.collapse('show');
+        if ( (userHasScrolledRight && sidebarShouldBeScrolling) || (!sidebarShouldBeScrolling &&  sidebarIsAttached) ) {
+            detachSidebar( sidebarID );
+        } else if(sidebarShouldBeScrolling && !sidebarIsAttached) {
+            attachSidebar( sidebarID );
+        } 
+    }
+
+    var detachSidebar = function ( sidebarID ) {
+        $( sidebarID ).attr('class', 'affix-top');
+    }
+
+    var attachSidebar = function ( sidebarID ) {
+        $( sidebarID ).attr('class', 'affix');
+    }
+
+    var openCollapsedHashedElements = function () {
+        // Open a hashed element if it is collapsed.  This is a little trickier than it sounds,
+        // because then we also have to open any parents that are also collapsed.
+        if (location.hash) {
+            var $elementToOpen = $(location.hash);
+            if ($elementToOpen.is('.collapse:not(.in)')) {
+                $elementToOpen.parents('.collapse:not(.in)').collapse('show');
+                $elementToOpen.collapse('show');
+            }
         }
     }
+
+    var setupDocumentationPanels = function () {
+        var documentationPanelInformation = [ [ '.showSection2', '#section2Content', '#section2' ],
+                                              [ '.showSection3', '#section3Content', '#section3' ] ];
+        documentationPanelInformation.forEach( function ( panelInfo ) {
+            configurePanelBehavior.apply(null, panelInfo);
+        });
+    }
+
+    var configurePanelBehavior = function (linkToPanel, panelID, targetHref) {
+        $( panelID ).on('shown.bs.collapse', function () {
+            focusWindowOn( targetHref );
+        });
+        changePageFocusEvenIfPanelsOpen(linkToPanel, panelID, targetHref);
+    };
+
+    var changePageFocusEvenIfPanelsOpen = function (linkToPanel, panelID, targetHref) {
+        $( linkToPanel ).on('click', function () {
+            openPanel( panelID );
+
+            var panelWasClosed = $( panelID ).attr('class') === 'panel-collapse collapse in';
+            if( panelWasClosed ) {
+                focusWindowOn( targetHref );
+            };
+        });
+    }
+
+    var focusWindowOn = function ( location ) {
+        window.location.href = location;
+    }
+
+    var openPanel = function ( panelID ) {
+        $( panelID ).collapse('show');
+    }
+
+    initializePage();
+
 });
