9887468492ce761191f913f0bca68ded5417fcca
Anu <anu.varshneya@gmail.com>
2015-10-12 13:27:06 -0700

    Warning moldule adjusts to screen size

--- web-client/public/js/upload.js
+++ web-client/public/js/upload.js
@@ -110,6 +110,19 @@ $(function () {
       warningsString += warnings[i].errorDescription + " <br><br>";
     }
     $("#warningsList").html(warningsString);
+
+    var screenHeight = $(window).height();
+    var MIN_SCREEN_HEIGHT = 600;
+    var BORDER = 425;
+    var setPanel = screenHeight-BORDER+"px";
+    var minPanel = MIN_SCREEN_HEIGHT-BORDER+"px";
+    if (screenHeight > MIN_SCREEN_HEIGHT) {
+      $("#list-frame").css({height: setPanel});
+    } else {
+      $("#list-frame").css({height: minPanel});
+    }
+
+
     $("#warningsModal").modal("show");
   }
 
