4bb832a18ff5822aab7c46583ac898aef3b120f7
Anu <anu.varshneya@gmail.com>
2015-11-19 16:34:22 -0800

    fixed the warning unit test under gene-name-modifications

--- test/warnings-gene-name-modifications.js
+++ test/warnings-gene-name-modifications.js
@@ -7,12 +7,8 @@ var spreadsheetController = require(__dirname + '/../server/controllers' + '/spr
 describe('warnings-gene-name-modifications', function () {
 	describe('NaN-as-gene-name', function () {
     it('should not return any warnings', function () {
-      var sheet = xlsx.parse('NaN-as-gene-name-input.xlsx'),
-      network = spreadsheetController.parseSheet(sheet);
-
-  		assert.equal(0, network.warnings.length);
-      //test.duplicateGeneError('test-files/gene-name-modifications/NaN-as-gene-name-input.xlsx');
-      //test.duplicateGeneError('test-files/gene-name-modifications/NaN-as-gene-name-output.xlsx');
+      test.noWarnings('test-files/gene-name-modifications/NaN-as-gene-name-input.xlsx');
+      test.noWarnings('test-files/gene-name-modifications/NaN-as-gene-name-output.xlsx');
     })
   })
 })
