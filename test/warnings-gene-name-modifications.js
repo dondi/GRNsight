ba30115fa2917b382c8221f3e0412d8d04a94ee8
Anu <anu.varshneya@gmail.com>
2015-12-23 18:18:41 -0800

    initial gene-name unit tests

--- test/warnings-gene-name-modifications.js
+++ test/warnings-gene-name-modifications.js
@@ -6,9 +6,9 @@ var spreadsheetController = require(__dirname + '/../server/controllers' + '/spr
 
 describe('warnings-gene-name-modifications', function () {
 	describe('NaN-as-gene-name', function () {
-    it('should not return any warnings', function () {
-      test.noWarnings('test-files/gene-name-modifications/NaN-as-gene-name-input.xlsx');
-      test.noWarnings('test-files/gene-name-modifications/NaN-as-gene-name-output.xlsx');
-    })
-  })
+    	it('should not return any warnings', function () {
+      		test.noWarnings('test-files/gene-name-modifications/NaN-as-gene-name-input.xlsx');
+      		test.noWarnings('test-files/gene-name-modifications/NaN-as-gene-name-output.xlsx');
+    	})
+  	})
 })
