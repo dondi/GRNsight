c0d1655d08e8036aef7ded4cc2781ac0fa16abcb
Anu <anu.varshneya@gmail.com>
2016-01-13 10:59:40 -0800

    Reorganized unit tests alphabetically by file name

--- test/warnings-gene-name-modifications.js
+++ test/warnings-gene-name-modifications.js
@@ -5,7 +5,50 @@ var spreadsheetController = require(__dirname + '/../server/controllers' + '/spr
 
 
 describe('warnings-gene-name-modifications', function () {
-	describe('NaN-as-gene-name', function () {
+  	describe('mismatched-case-related', function () {
+    	it('should not return any warnings', function () {
+      		test.noWarnings('test-files/gene-name-modifications/mismatched-case-related-input.xlsx');
+      		test.noWarnings('test-files/gene-name-modifications/mismatched-case-related-output.xlsx');
+    	})
+  	})
+
+  	describe('mismatched-case-unrelated', function () {
+    	it('should not return any warnings', function () {
+      		test.noWarnings('test-files/gene-name-modifications/mismatched-case-unrelated-input.xlsx');
+      		test.noWarnings('test-files/gene-name-modifications/mismatched-case-unrelated-output.xlsx');
+    	})
+  	})
+
+  	describe('missing-gene-name-side', function () {
+    	it('return 1 target and 4 randon data warnings per file', function () {
+      		test.missingTargetWarning('test-files/gene-name-modifications/missing-gene-name-side-input.xlsx',1);
+      		test.randomDataWarning('test-files/gene-name-modifications/missing-gene-name-side-input.xlsx',4);
+      		test.missingTargetWarning('test-files/gene-name-modifications/missing-gene-name-side-output.xlsx',1);
+      		test.randomDataWarning('test-files/gene-name-modifications/missing-gene-name-side-output.xlsx',4);
+    	})
+  	})
+
+  	describe('missing-gene-name-top-and-side', function () {
+    	it('return 1 target 1 source and 5 randon data warnings per file', function () {
+      		test.missingTargetWarning('test-files/gene-name-modifications/missing-gene-name-top-and-side-input.xlsx',1);
+      		test.missingSourceWarning('test-files/gene-name-modifications/missing-gene-name-top-and-side-input.xlsx',1)
+      		test.randomDataWarning('test-files/gene-name-modifications/missing-gene-name-top-and-side-input.xlsx',5);
+      		test.missingTargetWarning('test-files/gene-name-modifications/missing-gene-name-top-and-side-output.xlsx',1);
+      		test.missingSourceWarning('test-files/gene-name-modifications/missing-gene-name-top-and-side-output.xlsx',1)
+      		test.randomDataWarning('test-files/gene-name-modifications/missing-gene-name-top-and-side-output.xlsx',5);
+    	})
+  	})
+
+  	describe('missing-gene-name-top', function () {
+    	it('return 1 source and 1 randon data warnings per file', function () {
+      		test.missingSourceWarning('test-files/gene-name-modifications/missing-gene-name-top-input.xlsx',1);
+      		test.randomDataWarning('test-files/gene-name-modifications/missing-gene-name-top-input.xlsx',1);
+      		test.missingSourceWarning('test-files/gene-name-modifications/missing-gene-name-top-output.xlsx',1);
+      		test.randomDataWarning('test-files/gene-name-modifications/missing-gene-name-top-output.xlsx',1);
+    	})
+  	})
+
+  	describe('NaN-as-gene-name', function () {
     	it('should not return any warnings', function () {
       		test.noWarnings('test-files/gene-name-modifications/NaN-as-gene-name-input.xlsx');
       		test.noWarnings('test-files/gene-name-modifications/NaN-as-gene-name-output.xlsx');
