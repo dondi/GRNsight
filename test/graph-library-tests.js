b869eebe72d5635fb4f9a68e0a1086cce2d3cef0
Mihir Samdarshi <mihirsamdarshi@yahoo.com>
2016-04-13 12:02:52 -0700

    Added GRNSight to Cytoscape converter function, made sure Dijkstra passses

--- test/graph-library-tests.js
+++ test/graph-library-tests.js
@@ -1,22 +1,52 @@
 var assert = require('chai').assert,
     xlsx = require('node-xlsx'),
-    cy = require('cytoscape'),
+    cytoscape = require('cytoscape'),
     test = require('./test');
 var spreadsheetController = require(__dirname + '/../server/controllers' + '/spreadsheet-controller')();
 
+//converts GRNSight object to an array accepted by Cytoscape
+
+var grnSightToCytoscape = function (network) {
+  var result = [];
+  network.genes.forEach(function (gene) {
+    result.push({
+      data: {
+        id: gene.name
+      }
+    })
+  });
+
+  network.links.forEach(function (link) {
+    var sourceGene = network.genes[link.source];
+    var targetGene = network.genes[link.target];
+    result.push({
+      data: {
+        id: sourceGene.name + targetGene.name,
+        source: sourceGene.name,
+        target: targetGene.name
+      }
+    })
+  });
+
+  return result;
+};
 
 describe('graph-library-tests', function() {
     describe('basic-cytoscape-conversion', function() {
         it('convert to cytoscape correctly', function() {
             var input = 'test-files/graph-statistics-tests/graph-stats-demo.xlsx';
-            var sheet = xlsx.parse(input),
-                network = spreadsheetController.parseSheet(sheet);
+            var sheet = xlsx.parse(input);
+            var network = spreadsheetController.parseSheet(sheet);
+            var cytoscapeElements = grnSightToCytoscape(network);
+            console.log(cytoscapeElements);
+//require calls cytoscape as a function so the below code is needed to call cytoscape
+            var cy = cytoscape({
+              headless: true,
+              elements: cytoscapeElements
+            })
 
-            console.log(cy == true);
-
-            /*var dijkstra = cy.dijkstra(function() {});
-            var distToJ = dijkstra.distanceTo(cy.network.genes[0]);
-            console.log(distToJ);*/
+            var dijkstra = cy.elements().dijkstra("#a");
+            console.log(dijkstra.distanceTo("#f"));
         })
     })
 });
