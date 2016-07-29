90ed20c1c105190f70beb339ae9620e851cd96cf
dondi <dondi@lmu.edu>
2016-07-29 15:21:00 -0700

    Implement weighted SIF export.

--- server/controllers/export-controller.js
+++ server/controllers/export-controller.js
@@ -1,22 +1,41 @@
+var exportUnweightedEdges = function (network, gene, geneIndex) {
+  var result = gene.name;
+
+  var linkAdded = false;
+  network.links.forEach(function (link) {
+    if (link.source === geneIndex) {
+      if (!linkAdded) {
+        result += "\tpd";
+        linkAdded = true;
+      }
+
+      result += "\t" + network.genes[link.target].name;
+    }
+  });
+
+  result += "\n";
+  return result;
+};
+
+var exportWeightedEdges = function (network, gene, geneIndex) {
+  var result = "";
+
+  network.links.forEach(function (link) {
+    if (link.source === geneIndex) {
+      result += [gene.name, link.value, network.genes[link.target].name].join("\t") + "\n";
+    }
+  });
+
+  return result || (gene.name + "\n");
+};
+
 var grnsightToSif = function (network) {
   var result = "";
 
   network.genes.forEach(function (gene, geneIndex) {
-    result += gene.name;
-
-    var linkAdded = false;
-    network.links.forEach(function (link) {
-      if (link.source === geneIndex) {
-        if (!linkAdded) {
-          result += "\tpd";
-          linkAdded = true;
-        }
-
-        result += "\t" + network.genes[link.target].name;
-      }
-    });
-
-    result += "\n";
+    result += network.sheetType === "unweighted" ?
+      exportUnweightedEdges(network, gene, geneIndex) :
+      exportWeightedEdges(network, gene, geneIndex);
   });
 
   return result;
--- test/export-tests.js
+++ test/export-tests.js
@@ -27,7 +27,35 @@ var unweightedTestNetwork = {
   "sheetType": "unweighted"
 };
 
-// TODO weightedTestNetwork
+var weightedTestNetwork = {
+  "genes": [
+    { "name": "A" },
+    { "name": "B" },
+    { "name": "C" },
+    { "name": "D" }
+  ],
+  "links": [
+    {
+      "source": 2,
+      "target": 1,
+      "value": 0.5
+    },
+    {
+      "source": 1,
+      "target": 0,
+      "value": -0.75
+    },
+    {
+      "source": 1,
+      "target": 2,
+      "value": 0.25
+    }
+  ],
+  "errors": [],
+  "warnings": [],
+  "sheetType": "weighted"
+};
+
 // TODO unweightedTestNetworkWithCycle
 // TODO weightedTestNetworkWithCycle
 
@@ -38,5 +66,14 @@ describe("Export to SIF", function () {
     expect(sifLines[1].split("\t")).to.deep.equal([ "B", "pd", "A", "C" ]);
     expect(sifLines[2].split("\t")).to.deep.equal([ "C", "pd", "B" ]);
     expect(sifLines[3]).to.equal("D");
+  });
+
+  it("should export weighted networks to SIF correctly", function () {
+    var sifLines = exportController.grnsightToSif(weightedTestNetwork).split("\n");
+    expect(sifLines[0]).to.equal("A");
+    expect(sifLines[1].split("\t")).to.deep.equal([ "B", "-0.75", "A" ]);
+    expect(sifLines[2].split("\t")).to.deep.equal([ "B", "0.25", "C" ]);
+    expect(sifLines[3].split("\t")).to.deep.equal([ "C", "0.5", "B" ]);
+    expect(sifLines[4]).to.equal("D");
   });
 });
