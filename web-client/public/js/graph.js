7d4f354e76623230853c038333c934af2e4b1b16
NAnguiano <nanguia1@lion.lmu.edu>
2016-11-07 13:48:03 -0800

    Got zoom to work.

--- web-client/public/js/graph.js
+++ web-client/public/js/graph.js
@@ -79,9 +79,71 @@ var drawGraph = function (nodes, links, positiveWeights, negativeWeights, sheetT
       //.on("drag", dragmove)
       //.on("dragend", dragend);
 
+  var zoom = d3.behavior.zoom().scaleExtent([1, 2]).on("zoom", zoomed);
+
   var svg = d3.select($container[0]).append("svg")
       .attr("width", width)
-      .attr("height", height);
+      .attr("height", height)
+      .append("g"); // required for zoom to work
+
+  function zoomed() {
+    svg.attr("transform",
+        "translate(" + zoom.translate() + ")" +
+        "scale(" + zoom.scale() + ")"
+    );
+  }
+
+  function interpolateZoom (translate, scale) {
+    var self = this;
+    return d3.transition().duration(350).tween("zoom", function () {
+        var iTranslate = d3.interpolate(zoom.translate(), translate),
+            iScale = d3.interpolate(zoom.scale(), scale);
+        return function (t) {
+            zoom
+                .scale(iScale(t))
+                .translate(iTranslate(t));
+            zoomed();
+        };
+    });
+  }
+
+  function zoomClick() {
+      var clicked = d3.event.target,
+          direction = 1,
+          factor = 0.2,
+          target_zoom = 1,
+          center = [width / 2, height / 2],
+          extent = zoom.scaleExtent(),
+          translate = zoom.translate(),
+          translate0 = [],
+          l = [],
+          view = {x: translate[0], y: translate[1], k: zoom.scale()};
+
+      d3.event.preventDefault();
+      direction = (this.id === 'zoom_in') ? 1 : -1;
+      target_zoom = zoom.scale() * (1 + factor * direction);
+
+      if (target_zoom < extent[0] || target_zoom > extent[1]) { 
+        if(zoom.scale() !== extent[0] && target_zoom < extent[0]) {
+          target_zoom = 1;
+        } else if (zoom.scale() !== extent[1] && target_zoom > extent[1]) {
+          target_zoom = 2;
+        } else {
+          return false;
+        }
+      }
+
+      translate0 = [(center[0] - view.x) / view.k, (center[1] - view.y) / view.k];
+      view.k = target_zoom;
+      l = [translate0[0] * view.k + view.x, translate0[1] * view.k + view.y];
+
+      view.x += center[0] - l[0];
+      view.y += center[1] - l[1];
+
+      interpolateZoom([view.x, view.y], view.k);
+  }
+
+  d3.selectAll('button').on('click', zoomClick);
 
   var defs = svg.append("defs");
 
