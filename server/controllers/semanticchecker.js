74eb4ca4196e80f592bafef972fd991ca7e300ea
yshin4 <yshin4@lion.lmu.edu>
2016-11-07 15:51:31 -0800

    moved semantic errors in the errorList from spreadsheet-controller to semanticchecker

--- server/controllers/semanticchecker.js
+++ server/controllers/semanticchecker.js
@@ -18,7 +18,55 @@ var checkGeneLength = function(errorArray, genesList) {
 
 var checkSpecialCharacter = function (currentGene){
   var regex = /[^a-z0-9\_\-]/gi;
-  return currentGene.match(regex)===null;
+  return !currentGene.match(regex);
 }
 
   checkGeneLength(network.errors, genesList);
+
+
+var errorList = {
+
+  duplicateGeneError: function(geneType, geneName) {
+    return {
+      errorCode: "DUPLICATE_GENE",
+      possibleCause: "There exists a duplicate for " + geneType + " gene " + geneName + ".",
+      suggestedFix: "Please remove the duplicate gene and submit again."
+    };
+  },
+
+  geneLengthError: function (geneName) {
+    return {
+      errorCode: "INVALID_GENE_LENGTH",
+      possibleCause: "Gene " + geneName + " is more than 12 characters in length.",
+      suggestedFix: "Genes may only be between 1 and 12 characters in length. Please shorten the name and submit again."
+    };
+  },
+
+  networkSizeError: function (genesLength, edgesLength) {
+    return {
+      errorCode: "INVALID_NETWORK_SIZE",
+      possibleCause: "This network has " + genesLength + " genes, and " + edgesLength + " edges.",
+      suggestedFix: "Networks may not have more than 75 genes or 150 edges. Please reduce the size of your network and try again."
+    };
+  },
+
+  specialCharacterError: function(row, column){
+    var colLetter = numbersToLetters[column];
+    var rowNum = row + 1;
+    return {
+      errorCode: "INVALID_CHARACTER",
+      possibleCause: "The value in cell " + colLetter + rowNum + " contains invalid character.",
+      suggestedFix: "Please ensure all values in the data does not contain special characters except for '-' and '_'."
+    }
+  },
+
+  warningsCountError: {
+    errorCode: "WARNINGS_OVERLOAD",
+    possibleCause: "This network has over 75 warnings.",
+    suggestedFix: "Please check the format of your spreadsheet with the guidlines outlined on the" +
+    "Documentation page and try again. If you fix these errors and try to upload again, there may be " +
+    "further errors detected. As a general approach for fixing the errors, consider copying and " +
+    "pasting just your adjacency matrix into a fresh Excel Workbook and saving it."
+  }
+  
+}
--- server/controllers/spreadsheet-controller.js
+++ server/controllers/spreadsheet-controller.js
@@ -370,40 +370,6 @@ var errorList = {
     };
   },
 
-  duplicateGeneError: function(geneType, geneName) {
-    return {
-      errorCode: "DUPLICATE_GENE",
-      possibleCause: "There exists a duplicate for " + geneType + " gene " + geneName + ".",
-      suggestedFix: "Please remove the duplicate gene and submit again."
-    };
-  },
-
-  geneLengthError: function (geneName) {
-    return {
-      errorCode: "INVALID_GENE_LENGTH",
-      possibleCause: "Gene " + geneName + " is more than 12 characters in length.",
-      suggestedFix: "Genes may only be between 1 and 12 characters in length. Please shorten the name and submit again."
-    };
-  },
-
-  networkSizeError: function (genesLength, edgesLength) {
-    return {
-      errorCode: "INVALID_NETWORK_SIZE",
-      possibleCause: "This network has " + genesLength + " genes, and " + edgesLength + " edges.",
-      suggestedFix: "Networks may not have more than 75 genes or 150 edges. Please reduce the size of your network and try again."
-    };
-  },
-
-  specialCharacterError: function(row, column){
-    var colLetter = numbersToLetters[column];
-    var rowNum = row + 1;
-    return {
-      errorCode: "INVALID_CHARACTER",
-      possibleCause: "The value in cell " + colLetter + rowNum + " contains invalid character.",
-      suggestedFix: "Please ensure all values in the data does not contain special characters except for '-' and '_'."
-    }
-  },
-
   dataTypeError: function (row, column) {
     var colLetter = numbersToLetters[column];
     var rowNum = row + 1;
@@ -438,15 +404,6 @@ var errorList = {
   errorsCountError: {
     errorCode: "ERRORS_OVERLOAD",
     possibleCause: "This network has over 20 errors.",
-    suggestedFix: "Please check the format of your spreadsheet with the guidlines outlined on the" +
-    "Documentation page and try again. If you fix these errors and try to upload again, there may be " +
-    "further errors detected. As a general approach for fixing the errors, consider copying and " +
-    "pasting just your adjacency matrix into a fresh Excel Workbook and saving it."
-  },
-
-  warningsCountError: {
-    errorCode: "WARNINGS_OVERLOAD",
-    possibleCause: "This network has over 75 warnings.",
     suggestedFix: "Please check the format of your spreadsheet with the guidlines outlined on the" +
     "Documentation page and try again. If you fix these errors and try to upload again, there may be " +
     "further errors detected. As a general approach for fixing the errors, consider copying and " +
