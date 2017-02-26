2c84b87a122aa7db7da7e1a4340b50a9ebc29031
Eileen Choe <echoe@lion.lmu.edu>
2017-02-26 14:02:59 -0800

    sif format error (no tabs)

--- server/controllers/importers/sif.js
+++ server/controllers/importers/sif.js
@@ -1,9 +1,6 @@
 var constants = require(__dirname + "/../constants");
 var semanticChecker = require(__dirname + "/../semantic-checker");
 
-// Eventually wrap the final return in the semanticChecker
-// semanticChecker(network);
-
 var GENE_NAME = 0;
 var RELATIONSHIP = 1;
 var TARGET = 2;
@@ -24,11 +21,28 @@ var sifNetworkType = function (sifEntries) {
   var allNumbers = relationships.every(isNumber);
   return {
     sheetType: allNumbers ? constants.WEIGHTED : constants.UNWEIGHTED,
-    warnings: hasNumbers && !allNumbers ? [ constants.warnings.EDGES_WITHOUT_WEIGHTS ] : []
+    warnings: hasNumbers && !allNumbers ? constants.warnings.EDGES_WITHOUT_WEIGHTS : null
   };
 };
 
 module.exports = function (sif) {
+
+  var warnings = [];
+  var errors = [];
+
+  var errorList = {
+    sifFormatError: {
+      errorCode: "SIF_FORMAT_ERRROR",
+      possibleCause: "No tabs are detected in the SIF input file",
+      suggestedFix: "SIF files accepted by GRNsight must delimit data using tabs. Please review the SIF input standards" +
+      "that are outlined in the documentation."
+    }
+  };
+
+  if (!sif.match(/[\t]+/g)) {
+    errors.push(errorList.sifFormatError);
+  }
+
   var entries = sif.match(/[^\r\n]+/g).map(function (line) {
     return line.match(/[^\t]+/g);
   });
@@ -41,6 +55,9 @@ module.exports = function (sif) {
   });
 
   var networkType = sifNetworkType(entries);
+  if (networkType.warnings !== null) {
+    warnings.push(networkType.warnings);
+  }
 
   var links = [];
   entries.forEach(function (entry) {
@@ -68,14 +85,16 @@ module.exports = function (sif) {
     }
   });
 
-  var network =
-  {
+  console.log(warnings);
+  console.log(errors);
+
+  var network = {
     genes: genes.map(function (geneName) {
       return { name: geneName };
     }),
     links: links,
-    errors: [],
-    warnings: networkType.warnings,
+    errors: errors,
+    warnings: warnings,
     sheetType: networkType.sheetType,
     positiveWeights: [],
     negativeWeights: []
