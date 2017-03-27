b5c42cf62476caf5b05d62487780359e078d210b
NAnguiano <nanguia1@lion.lmu.edu>
2017-03-27 12:19:48 -0700

    Remove some unnecessary code. Also clean up weird scrollbars. Initial starts for left/right/zoom buttons.

--- web-client/public/js/container.js
+++ web-client/public/js/container.js
@@ -1,13 +1,12 @@
 $(function () {
   var grnTest = $(".grnTest");
-  var grnParent = $(".grnParent");
   var container = $(".grnsight-container");
   var enableScroll = $("#enableScroll");
   var pageWidth = $(window).width();
 
-  if (pageWidth < 1300) {
+  if (pageWidth < 1500) {
       $('#boundBoxS').prop('checked', true);
-  } else if (pageWidth > 1300 && pageWidth < 2300) {
+  } else if (pageWidth > 1500 && pageWidth < 2300) {
       $('#boundBoxM').prop('checked', true);
   } else {
       $('#boundBoxL').prop('checked', true);
@@ -22,26 +21,6 @@ $(function () {
     if (!container.hasClass(currentValue)) {
       container.attr("class", grnsightContainerClass);
       $("#reload").trigger("click");
-    //   if (currentValue === "containerInfinite") {
-    //       grnTest.css("height", "900px");
-    //       grnTest.css("width", "1600px");
-    //       let halfVisibleHeight =
-    //             (container.height() / 2) - (grnTest.height() / 2);
-    //       let halfVisibleWidth =
-    //             (container.width() / 2) - (grnTest.width() / 2);
-    //       grnTest.scrollTop(halfVisibleHeight);
-    //       grnTest.scrollLeft(halfVisibleWidth);
-    //       grnTest.css("overflow", "auto");
-    //       grnTest.addClass("grnTestNoScroll");
-    //       enableScroll.prop("disabled", true);
-    //       grnParent.css("overflow", "hidden")
-    //   } else if ($(".grnTest").hasClass("grnTestNoScroll")) {
-    //       grnTest.removeClass("grnTestNoScroll");
-    //       enableScroll.prop("disabled", false);
-    //       enableScroll.trigger("click");
-    //       enableScroll.trigger("click");
-    //       grnParent.css("overflow", "auto")
-    //   }
     };
   });
 
