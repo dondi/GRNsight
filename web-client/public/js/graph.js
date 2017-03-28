78b013bb3ff294e3f63502397560758331a0b909
NAnguiano <nanguia1@lion.lmu.edu>
2017-03-27 17:39:40 -0700

    Nevermind, zoom is broken.

--- web-client/public/js/graph.js
+++ web-client/public/js/graph.js
@@ -87,73 +87,73 @@ var drawGraph = function (nodes, links, positiveWeights, negativeWeights, sheetT
       .append("g"); // required for zoom to work
 
   /* Credit to http://bl.ocks.org/linssen/7352810 for zoom on center */
-  var zoom = d3.behavior.zoom()
-      .center([width / 2, height / 2])
-      .scaleExtent([0, 5])
-      .on("zoom", zoomed);
-
-  svg.call(zoom);
-
-  function zoomed() {
-    svg.attr("transform",
-        "translate(" + zoom.translate() + ")" +
-        "scale(" + zoom.scale() + ")"
-    );
-  }
-
-  function interpolateZoom (translate, scale) {
-    var self = this;
-    return d3.transition().duration(350).tween("zoom", function () {
-        var iTranslate = d3.interpolate(zoom.translate(), translate),
-            iScale = d3.interpolate(zoom.scale(), scale);
-        return function (t) {
-          console.log(iScale(t));
-            zoom
-                .scale(iScale(t))
-                .translate(iTranslate(t));
-            zoomed();
-        };
-    });
-  }
-
-  function zoomClick() {
-    var clicked = d3.event.target,
-        direction = 1,
-        factor = 0.2,
-        target_zoom = 0,
-        center = [width / 2, height / 2],
-        extent = zoom.scaleExtent(),
-        translate = zoom.translate(),
-        translate0 = [],
-        l = [],
-        view = {x: translate[0], y: translate[1], k: zoom.scale()};
-
-    d3.event.preventDefault();
-    direction = (this.id === 'zoomIn') ? 1 : -1;
-    target_zoom = zoom.scale() * (1 + factor * direction);
-
-    if (target_zoom < extent[0] || target_zoom > extent[1]) {
-      if(zoom.scale() !== extent[0] && target_zoom < extent[0]) {
-        target_zoom = 0;
-      } else if (zoom.scale() !== extent[1] && target_zoom > extent[1]) {
-        target_zoom = 10;
-      } else {
-        return false;
-      }
-    }
-
-    translate0 = [(center[0] - view.x) / view.k, (center[1] - view.y) / view.k];
-    view.k = target_zoom;
-    l = [translate0[0] * view.k + view.x, translate0[1] * view.k + view.y];
-
-    view.x += center[0] - l[0];
-    view.y += center[1] - l[1];
-
-    interpolateZoom([view.x, view.y], view.k);
-  }
-
-  d3.selectAll('.zoomBtn')
-    .on('click', zoomClick);
+  /* TODO Check this method https://bl.ocks.org/mbostock/7ec977c95910dd026812 */
+  // var zoom = d3.behavior.zoom()
+  //     .scaleExtent([0, 5])
+  //     .on("zoom", zoomed);
+  //
+  // svg.call(zoom);
+  //
+  // function zoomed() {
+  //   svg.attr("transform",
+  //       "translate(" + zoom.translate() + ")" +
+  //       "scale(" + zoom.scale() + ")"
+  //   );
+  // }
+  //
+  // function interpolateZoom (translate, scale) {
+  //   var self = this;
+  //   return d3.transition().duration(350).tween("zoom", function () {
+  //       var iTranslate = d3.interpolate(zoom.translate(), translate),
+  //           iScale = d3.interpolate(zoom.scale(), scale);
+  //       return function (t) {
+  //         console.log(iScale(t));
+  //           zoom
+  //               .scale(iScale(t))
+  //               .translate(iTranslate(t));
+  //           zoomed();
+  //       };
+  //   });
+  // }
+  //
+  // function zoomClick() {
+  //   var clicked = d3.event.target,
+  //       direction = 1,
+  //       factor = 0.2,
+  //       target_zoom = 0,
+  //       center = [width / 2, height / 2],
+  //       extent = zoom.scaleExtent(),
+  //       translate = zoom.translate(),
+  //       translate0 = [],
+  //       l = [],
+  //       view = {x: translate[0], y: translate[1], k: zoom.scale()};
+  //
+  //   d3.event.stopPropagation();
+  //   direction = (this.id === 'zoomIn') ? 1 : -1;
+  //   target_zoom = zoom.scale() * (1 + factor * direction);
+  //
+  //   if (target_zoom < extent[0] || target_zoom > extent[1]) {
+  //     if(zoom.scale() !== extent[0] && target_zoom < extent[0]) {
+  //       target_zoom = 0;
+  //     } else if (zoom.scale() !== extent[1] && target_zoom > extent[1]) {
+  //       target_zoom = 10;
+  //     } else {
+  //       return false;
+  //     }
+  //   }
+  //
+  //   translate0 = [(center[0] - view.x) / view.k, (center[1] - view.y) / view.k];
+  //   view.k = target_zoom;
+  //   l = [translate0[0] * view.k + view.x, translate0[1] * view.k + view.y];
+  //
+  //   view.x += center[0] - l[0];
+  //   view.y += center[1] - l[1];
+  //
+  //   interpolateZoom([view.x, view.y], view.k);
+  // }
+  //
+  // d3.selectAll('.zoomBtn')
+  //   .on('click', zoomClick);
 
   var defs = svg.append("defs");
 
