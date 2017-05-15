b465139807b683d8f1c3b975888c3ac6ab75d8ed
NAnguiano <nanguia1@lion.lmu.edu>
2017-05-14 23:01:19 -0700

    ESLint...

--- server/config/config.js
+++ server/config/config.js
@@ -1,37 +1,37 @@
-var path = require('path'),
-    rootPath = path.normalize(__dirname + '/..');
+var path = require("path");
+var rootPath = path.normalize(__dirname + "/..");
 
 module.exports = {
-  development: {
-    host: 'localhost',
-    port: 5000,
-    url: 'http://' + this.host + '/' + this.port,
-    corsOrigin: 'http://localhost:5001',
-    root: rootPath,
-    app: {
-      name: 'GRNsight'
-    }    
-  },
+    development: {
+        host: "localhost",
+        port: 5000,
+        url: "http://" + this.host + "/" + this.port,
+        corsOrigin: "http://localhost:5001",
+        root: rootPath,
+        app: {
+            name: "GRNsight"
+        }
+    },
 
-  production: {
-    host: 'grnsight.cs.lmu.edu',
-    port: 3000,
-    url: 'http://' + this.host + '/server',
-    corsOrigin: 'http://grnsight.cs.lmu.edu',
-    root: rootPath,
-    app: {
-      name: 'GRNsight'
-    }
-  },
+    production: {
+        host: "grnsight.cs.lmu.edu",
+        port: 3000,
+        url: "http://" + this.host + "/server",
+        corsOrigin: "http://grnsight.cs.lmu.edu",
+        root: rootPath,
+        app: {
+            name: "GRNsight"
+        }
+    },
 
-  beta: {
-    host: 'grnsight.cs.lmu.edu',
-    port: 4000,
-    url: 'http://' + this.host + '/beta/server/',
-    corsOrigin: 'http://grnsight.cs.lmu.edu',
-    root: rootPath,
-    app: {
-      name: 'GRNsight'
+    beta: {
+        host: "grnsight.cs.lmu.edu",
+        port: 4000,
+        url: "http://" + this.host + "/beta/server/",
+        corsOrigin: "http://grnsight.cs.lmu.edu",
+        root: rootPath,
+        app: {
+            name: "GRNsight"
+        }
     }
-  }
-}
+};
