0cfdc234cabcf3237f498095a0810cb3e3061ceb
cazinge <eddie.azinge@gmail.com>
2017-11-30 15:57:18 -0800

    first gene database helper function working

--- web-client/public/js/api.js
+++ web-client/public/js/api.js
@@ -2,9 +2,28 @@
 
 // Using "use strict" global
 
-(function () {
+(function ($) {
     window.api = {
-        getGeneInformation: function () {
+        getGeneInformation: function (symbol) {
+            var getUniProtInfo = function (geneSymbol) {
+                return $.get("http://www.uniprot.org/uploadlists/", {
+                    from: "GENENAME",
+                    to: "ACC",
+                    format: "tab",
+                    taxon: "559292",
+                    query: geneSymbol
+                }).then(function (data) {
+                    var regex = new RegExp(geneSymbol + "[ \t\r\n\v\f]*([A-Z0-9]+)", "gm");
+                    var id = regex.exec(data)[1];
+                    return $.get({
+                        url: "http://www.uniprot.org/uniprot/" + id + ".xml",
+                        crossDomain: true,
+                    });
+                }).then(function (info) {
+                    console.log(info);
+                });
+            };
+            getUniProtInfo(symbol);
         }
     };
-})();
+})($);
