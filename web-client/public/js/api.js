a73000a5af09e0dfc55a8eb4627c775badd7a264
cazinge <eddie.azinge@gmail.com>
2017-11-30 16:29:35 -0800

    finished 3 of 5 database functions

--- web-client/public/js/api.js
+++ web-client/public/js/api.js
@@ -6,24 +6,92 @@
     window.api = {
         getGeneInformation: function (symbol) {
             var getUniProtInfo = function (geneSymbol) {
-                return $.get("http://www.uniprot.org/uploadlists/", {
-                    from: "GENENAME",
-                    to: "ACC",
-                    format: "tab",
-                    taxon: "559292",
-                    query: geneSymbol
+                return $.get({
+                    url: "http://www.uniprot.org/uploadlists/",
+                    data: {
+                        from: "GENENAME",
+                        to: "ACC",
+                        format: "tab",
+                        taxon: "559292",
+                        query: geneSymbol,
+                    },
+                    dataType: "text",
                 }).then(function (data) {
                     var regex = new RegExp(geneSymbol + "[ \t\r\n\v\f]*([A-Z0-9]+)", "gm");
                     var id = regex.exec(data)[1];
                     return $.get({
                         url: "http://www.uniprot.org/uniprot/" + id + ".xml",
-                        crossDomain: true,
                     });
-                }).then(function (info) {
-                    console.log(info);
                 });
             };
-            getUniProtInfo(symbol);
+
+            var getNCBIInfo = function (geneSymbol) {
+                return $.get({
+                    url: "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi",
+                    data: {
+                        db: "gene",
+                        term: geneSymbol + "[gene]+Saccharomyces+cerevisiae[Organism]",
+                    },
+                    dataType: "text",
+                }).then(function (data) {
+                    var regex = /<Id>(\d*)<\/Id>/gm;
+                    var id = regex.exec(data)[1];
+                    return $.get({
+                        url: "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=gene&id=" + id,
+                        dataType: "xml",
+                    });
+                });
+            };
+
+            var getYeastMineInfo = function (geneSymbol) {
+                // John Lopez
+            };
+
+            var getEnsemblInfo = function (geneSymbol) {
+                return $.get({
+                    url: "http://rest.ensembl.org/lookup/symbol/saccharomyces_cerevisiae/" + geneSymbol,
+                    dataType: "json",
+                    beforeSend: function (xhr) {
+                        xhr.setRequestHeader("content-type", "application/json");
+                    },
+                }).then(function (data) {
+                    return $.get({
+                        url: "http://rest.ensembl.org/lookup/id/" + data.id + "?expand=1",
+                        dataType: "json",
+                        beforeSend: function (xhr) {
+                            xhr.setRequestHeader("content-type", "application/json");
+                        },
+                    });
+                });
+            };
+
+            var getJasparInfo = function (geneSymbol) {
+                // JASPAR group
+            };
+
+            // change if any preprocessing needs to be done on the data before being given to the application
+            var filterData = function (data) {
+                return data;
+            };
+
+            // TODO: Error Handling, Mock Testing, Data filtering
+            return $.when(
+                getUniProtInfo(symbol),
+                getNCBIInfo(symbol),
+                getYeastMineInfo(symbol),
+                getEnsemblInfo(symbol),
+                getJasparInfo(symbol)
+            ).done(function (uniprotInfo, ncbiInfo, yeastmineInfo, ensemblInfo, jasparInfo) {
+                var data = {
+                    Uniprot: uniprotInfo,
+                    NCBI: ncbiInfo,
+                    "SGD/YeastMine": yeastmineInfo,
+                    Ensembl: ensemblInfo,
+                    JASPAR: jasparInfo,
+                };
+                console.log(data);
+                return filterData(data);
+            });
         }
     };
 })($);
