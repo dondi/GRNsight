cce31e444da0a1ec316543ef7a32750777c3e4f0
John Lopez <johnllopez616@gmail.com>
2017-12-02 16:36:38 -0800

    Added spacing

--- XML/XMLfunction.js
+++ XML/XMLfunction.js
@@ -4,15 +4,15 @@ var serializer = new XMLSerializer();
 var doStuff = function() {
 
     $.get("https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=gene&id=856321").done(response => {
-    var data = response.getElementsByTagName("OtherAliases")[0];
-    var result = serializer.serializeToString(data);
-    //creates an array for us to use
-    var arr = result.split(",");
-    $("#someElement").append("LOTUS Tag: " + arr[0]);
-    
-    //gets rid of the LOTUS tag
-    arr.splice(0,1);
-     $("#anotherElement").append("Also Known As: " + arr);
+        var data = response.getElementsByTagName("OtherAliases")[0];
+        var result = serializer.serializeToString(data);
+        //creates an array for us to use
+        var arr = result.split(",");
+        $("#someElement").append("LOTUS Tag: " + arr[0]);
+
+        //gets rid of the LOTUS tag
+        arr.splice(0,1);
+         $("#anotherElement").append("Also Known As: " + arr);
     });
 }
 
