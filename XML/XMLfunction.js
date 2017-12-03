62c9bbe1305381f67c29b9e65ea17ae323948f78
John Lopez <johnllopez616@gmail.com>
2017-12-02 16:33:32 -0800

    Parses LOTUS tag and AKA

--- XML/XMLfunction.js
+++ XML/XMLfunction.js
@@ -1,12 +1,19 @@
 //I know this code is trash but I'm just using this as a place to dump where I find the stuff that I'm working on.
+var serializer = new XMLSerializer();
 
 var doStuff = function() {
 
-$.get("https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=gene&id=853313").done(response => {
-     $("#someElement").append(response.getElementsByTagName("Name")[0]);
+    $.get("https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=gene&id=856321").done(response => {
+    var data = response.getElementsByTagName("OtherAliases")[0];
+    var result = serializer.serializeToString(data);
+    //creates an array for us to use
+    var arr = result.split(",");
+    $("#someElement").append("LOTUS Tag: " + arr[0]);
+    
+    //gets rid of the LOTUS tag
+    arr.splice(0,1);
+     $("#anotherElement").append("Also Known As: " + arr);
     });
 }
-
-
 
 window.onload = doStuff();
