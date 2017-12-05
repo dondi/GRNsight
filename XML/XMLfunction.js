0e0e9700d7345b944be574d75e48b40746c1b8fe
John Lopez <johnllopez616@gmail.com>
2017-12-04 19:14:11 -0800

    Made this file equivalent to the one on the master branch

--- XML/XMLfunction.js
+++ XML/XMLfunction.js
@@ -1,43 +1,47 @@
+var serializer = new XMLSerializer();
 
+var callNCBI = function() {
 
+    $.get("https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=gene&id=856321" ).done(response => {
+        var data = response.getElementsByTagName("OtherAliases")[0];
 
-//I know this code is trash but I'm just using this as a place to dump where I find the stuff that I'm working on.
+        var result = serializer.serializeToString(data);
+        //creates an array for us to use
+        var arr = result.split(",");
+        $("#someElement").append("LOTUS Tag: " + arr[0]);
 
-var xmlToString = xml => new XMLSerializer().serializeToString(xml);
+        //gets rid of the LOTUS tag
+        arr.splice(0,1);
+         $("#anotherElement").append("Also Known As: " + arr);
+    });
+}
+var callUniprot = function() {
 
+    $.get("http://www.uniprot.org/uniprot/P34567.xml").done(response => {
+      var protein = response.getElementsByTagName("protein")[0];
+    //  var proteinType = serializer.serializeToString(protein.firstChild);
+      var proteinType = serializer.serializeToString(protein.childNodes[1].childNodes[1]);
+      $("#proteinType").append("Protein Type/Name: "+ proteinType);
 
-var xml = "";
+      var organism = response.getElementsByTagName("organism")[0];
 
-var doStuff = function() {
+      var species = serializer.serializeToString(organism.childNodes[1]);
+      //Can we please cut the common name out???
+      var commonName = serializer.serializeToString(organism.childNodes[3]);
+      $("#species").append("Species: " + species + " Common Name:" + commonName );
 
-$.get("https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=gene&id=853313").done(response => {
-      $("#someElement").append(xmlToString(response));
-     // hideLoadingMessage("ncbi");
+      var sequence = serializer.serializeToString(response.getElementsByTagName("sequence")[0]);
+      $("#sequence").append("Sequence: " + sequence);
+
+      var uniprotID = serializer.serializeToString(response.getElementsByTagName("name")[0]);
+      var uniprotAccession = serializer.serializeToString(response.getElementsByTagName("accession")[0]);
+      $("#geneID").append("Gene ID: "  + uniprotAccession + " / " + uniprotID);
+
+      //TODO: Similar Proteins (Can't find where the info is coming from, help me please Eddie)
+
     });
 
-/*
-$.get("https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=gene&id=853313").done(response => {
-      console.log("NCBI", response);
-      xml = xmlToString(response);
-      hideLoadingMessage("ncbi");
-    });
-    */
+}
 
-
-   var xml = new (XMLSerializer()).serializeToString("https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=gene&id=853313"),
-      xmlDoc = $.parseXML( xml ),
-      $xml = $( xmlDoc ),
-      $title = $xml.find( "Name" );
-
-    // Append "RSS Title" to #someElement
-    $( "#someElement" ).append( $title.text() );
-
-    // Change the title to "XML Title"
-   // $title.text( "XML Title" );
-
-    // Append "XML Title" to #anotherElement
-    //$( "#anotherElement" ).append( $title.text() );
-    }
-
-
-window.onload = doStuff();
+window.onload = callNCBI();
+window.onload = callUniprot();
