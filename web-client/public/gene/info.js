3eb74f7ac7e4cbe4dc16a837b7b92d917ae64dd0
bhamilton18 <blairvham@gmail.com>
2017-12-07 15:41:21 -0800

    All ID tags have are custom to the gene clicked

--- web-client/public/gene/info.js
+++ web-client/public/gene/info.js
@@ -15,5 +15,18 @@
         var sgdHrefTemplate = "https://www.yeastgenome.org/locus/";
         var sgdId = gene.sgd.sgdID;
         $(".sgd-link").text(sgdId).attr({ href: sgdHrefTemplate + sgdId });
+
+        var ncbiHrefTemplate = "https://www.ncbi.nlm.nih.gov/gene/";
+        var ncbiId = gene.ncbi.ncbiID;
+        $(".ncbi-link").text(ncbiId).attr({ href: ncbiHrefTemplate + ncbiId });
+
+        var ensemblHrefTemplate = "https://www.ensembl.org/Saccharomyces_cerevisiae/Gene/" +
+        "/Summary?db=core;g=YFL039C;r=VI:53260-54696;t=";
+        var ensemblId = gene.emsembl.ensemblID;
+        $(".ensembl-link").text(ensemblId).attr({ href: ensemblHrefTemplate + ensemblId });
+
+        var uniprotHrefTemplate = "http://www.uniprot.org/uniprot/";
+        var uniprotId = gene.uniprot.uniprotID;
+        $(".uniprot-link").text(uniprotId).attr({ href: uniprotHrefTemplate + uniprotId });
     })
 })();
