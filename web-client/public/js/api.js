8a5465718be00a5d9cf3f6c0e40c06f9539541b9
John Lopez <johnllopez616@gmail.com>
2017-12-09 13:09:00 -0800

    Deleted XML function and added XML functions to api.js

--- web-client/public/js/api.js
+++ web-client/public/js/api.js
@@ -5,6 +5,7 @@
 (function ($) {
     window.api = {
         getGeneInformation: function (symbol) {
+            var serializer = new XMLSerializer();
             var getUniProtInfo = function (geneSymbol) {
                 return $.get({
                     url: "http://www.uniprot.org/uploadlists/",
@@ -44,17 +45,21 @@
             };
 
             var getYeastMineInfo = function (geneSymbol) {
-                // return $.get({
-                //     url: "https://yeastmine.yeastgenome.org/yeastmine/service/data/Gene?symbol=" + geneSymbol,
-                //     dataType: "json",
-                //     beforeSend: function (xhr) {
-                //         xhr.setRequestHeader("content-type", "application/json");
-                //     },
-                // }).then(function (data) {
-                //     return $.get({
-                //        // TODO: This part
-                //     });
-                // });
+                return $.get({
+                    url: "https://yeastmine.yeastgenome.org/yeastmine/service/data/Gene?symbol=" + geneSymbol,
+                    dataType: "json",
+                    beforeSend: function (xhr) {
+                        xhr.setRequestHeader("content-type", "application/json");
+                    },
+                }).then(function (data) {
+                    console.log(data.results.filter(function (data) {
+                        return data.primaryIdentifier[0] === "S";
+                    }));
+                    return(data.results.filter(function (data) {
+
+                        return data.primaryIdentifier[0] === "S";
+                    }));
+                });
             };
 
             var getEnsemblInfo = function (geneSymbol) {
@@ -77,15 +82,16 @@
 
             var getJasparInfo = function (geneSymbol) {
                 return $.get({
-                    url: "/jaspar/api/v1/matrix/?tax_id=4932&format=json&search=" + geneSymbol,
+                    url: "http://jaspar.genereg.net/api/v1/matrix/?tax_id=4932&search=" + geneSymbol,
                     dataType: "json",
                     beforeSend: function (xhr) {
                         xhr.setRequestHeader("content-type", "application/json");
                     },
                 }).then(function (data) {
-                    return (data.count === 0 ? {} :
+                    return (data.count === 0 ?
+                        null :
                         $.get({
-                            url: "/jaspar/api/v1/matrix/" + data.results[0].matrix_id,
+                            url: "http://jaspar.genereg.net/api/v1/matrix/" + data.results[0].matrix_id,
                             dataType: "json",
                             beforeSend: function (xhr) {
                                 xhr.setRequestHeader("content-type", "application/json");
@@ -97,40 +103,34 @@
 
             // change if any preprocessing needs to be done on the data before being given to the application
             var filterData = function (uniprotInfo, ncbiInfo, yeastmineInfo, ensemblInfo, jasparInfo) {
-                return {
-                    jaspar: !jasparInfo ? {} : {
-                        jasparID : jasparInfo.matrix_id, // string
-                        class: jasparInfo.class, // string
-                        family: jasparInfo.family, // array
-                        sequenceLogo: jasparInfo.sequence_logo, // string: URL to image
-                        frequencyMatrix: jasparInfo.pfm,  // object with keys ACIG, each key mapping to an array of ints
-                    },
-                    ncbi: {
+                var parseUniprot = function (data) {
+                    console.log(uniprotInfo[0]);
+                    return {
+                        uniprotID: serializer.serializeToString(data.getElementsByTagName("name")[0]),
+                        proteinSequence: serializer.serializeToString(data.getElementsByTagName("sequence")[0]),
+                        similarProtein: { name: "etc.", id: "etc." },
+                        proteinType:serializer.serializeToString(data.getElementsByTagName("protein")[0].childNodes[1].childNodes[1]),
+                        species: serializer.serializeToString(data.getElementsByTagName("organism")[0].childNodes[1]),
+                    }
+                }
+
+                var parseNCBI = function (data) {
+                    var tagArray = serializer.serializeToString(data.getElementsByTagName("OtherAliases")[0]).split(",");
+                    return {
                         ncbiID: "etc.",
-                        locusTag: "etc.",
-                        alsoKnownAs: "etc.",
+                        locusTag: tagArray[0],
+                        alsoKnownAs: tagArray.slice(1),
                         chromosomeSequence: "etc.",
                         genomicSequence: "etc.",
                         proteinSequence: "etc.",
-                    },
-                    emsembl: {
-                        ensemblID: "etc.",
-                        description: "etc.",
-                        dnaSequence: "etc.",
-                        geneLocation: "etc.",
-                        geneMap: "URL",
-                    },
-                    uniprot: {
-                        uniprotID: "etc.",
-                        proteinSequence: "etc.",
-                        similarProtein: { name: "etc.", id: "etc." },
-                        proteinType: "etc.",
-                        species: "etc.",
-                    },
-                    sgd: {
-                        sgdID: "etc.",
-                        standardName: "etc.",
-                        systematicName: "etc.",
+                    }
+                }
+
+                var parseYeastmine = function (data) {
+                    return {
+                        sgdID: data.primaryIdentifier, //string
+                        standardName: data.symbol, //string
+                        systematicName: data.secondaryIdentifier, //string
                         regulators: 1,
                         targets: 12,
                         totalInteractions: "etc.",
@@ -149,11 +149,38 @@
                         syntheticHaploinsufficiency: 1,
                         syntheticLethality: 6,
                         syntheticRescue: 11,
-                        geneOntologySummary: "etc.",
+                        geneOntologySummary: data.functionSummary, //string
                         molecularFunction: "etc.",
                         biologicalProcess: "etc.",
                         cellularComponent: "etc.",
-                    },
+                    }
+                }
+
+                var parseEnsembl = function (data) {
+                    return {
+                        ensemblID: "etc.",
+                        description: "etc.",
+                        dnaSequence: "etc.",
+                        geneLocation: "etc.",
+                        geneMap: "URL",
+                    }
+                }
+
+                var parseJaspar = function (data) {
+                    return {
+                        jasparID : data.matrix_id, // string
+                        class: data.class, // string
+                        family: data.family, // array
+                        sequenceLogo: data.sequence_logo, // string: URL to image
+                        frequencyMatrix: data.pfm,  // object with keys ACIG, each key mapping to an array of ints
+                    }
+                }
+                return {
+                    // jaspar: parseJaspar(),
+                    ncbi: parseNCBI(ncbiInfo[0]),
+                    emsembl: parseEnsembl(ensemblInfo[0]),
+                    uniprot: parseUniprot(uniprotInfo[0]) ,
+                    sgd: parseYeastmine(yeastmineInfo[0]),
                 };
             };
 
@@ -162,10 +189,10 @@
                 getUniProtInfo(symbol),
                 getNCBIInfo(symbol),
                 getYeastMineInfo(symbol),
-                getEnsemblInfo(symbol),
-                getJasparInfo(symbol)
-            ).then(function (uniprotInfo, ncbiInfo, yeastmineInfo, ensemblInfo, jasparInfo) {
-                return filterData(uniprotInfo, ncbiInfo, yeastmineInfo, ensemblInfo, jasparInfo[0]);
+                getEnsemblInfo(symbol)
+                // getJasparInfo(symbol)
+            ).done(function (uniprotInfo, ncbiInfo, yeastmineInfo, ensemblInfo, jasparInfo) {
+                return filterData(uniprotInfo, ncbiInfo, yeastmineInfo, ensemblInfo, jasparInfo);
             });
         }
     };
