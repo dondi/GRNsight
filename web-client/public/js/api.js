d1004e270a8b5da4e4598dd3e5ce2002aba706af
John Lopez <johnllopez616@gmail.com>
2017-12-13 00:12:05 -0800

    Added XML Parsing

--- web-client/public/js/api.js
+++ web-client/public/js/api.js
@@ -6,6 +6,9 @@
     window.api = {
         getGeneInformation: function (symbol) {
             var serializer = new XMLSerializer();
+            var XMLParser = function (data) {
+                return serializer.serializeToString(data).replace(/\<.*?\>\s?/g, "");
+            };
             var getUniProtInfo = function (geneSymbol) {
                 return $.get({
                     url: "http://www.uniprot.org/uploadlists/",
@@ -106,22 +109,24 @@
             var filterData = function (uniprotInfo, ncbiInfo, yeastmineInfo, ensemblInfo, jasparInfo) {
                 var parseUniprot = function (data) {
                     return {
-                        uniprotID: serializer.serializeToString(data.getElementsByTagName("name")[0]),
-                        proteinSequence: serializer.serializeToString(data.getElementsByTagName("sequence")[0]),
-                        proteinType: serializer.serializeToString(data.getElementsByTagName("protein")[0].childNodes[1].childNodes[1]),
-                        species: serializer.serializeToString(data.getElementsByTagName("organism")[0].childNodes[1]),
+                        uniprotID: XMLParser(data.getElementsByTagName("name")[0]),
+                        proteinSequence: XMLParser(data.getElementsByTagName("sequence")[0]),
+                        proteinType: XMLParser(data.getElementsByTagName("protein")[0].childNodes[1].childNodes[1]),
+                        species: XMLParser(data.getElementsByTagName("organism")[0].childNodes[1]),
                     };
                 };
 
                 var parseNCBI = function (data) {
                     var tagArray = serializer.serializeToString(data.getElementsByTagName("OtherAliases")[0]).split(",");
                     return {
-                        ncbiID: "etc.",
-                        locusTag: tagArray[0],
-                        alsoKnownAs: tagArray.slice(1),
-                        chromosomeSequence: "etc.",
-                        genomicSequence: "etc.",
-
+                        ncbiID: data.getElementsByTagName("DocumentSummary")[0].getAttribute("uid"),
+                        locusTag: tagArray[0].replace(/\<.*?\>\s?/g, ''),
+                        alsoKnownAs: tagArray.slice(1).join().replace(/\<.*?\>\s?/g, ''),
+                        chromosomeSequence: XMLParser(data.getElementsByTagName("ChrAccVer")[0]),
+                        genomicSequence: XMLParser(data.getElementsByTagName("ChrLoc")[0]) + "; "
+                        + XMLParser(data.getElementsByTagName("ChrAccVer")[0]) + " ("
+                        + XMLParser(data.getElementsByTagName("ChrStart")[0])
+                         + ".." + XMLParser(data.getElementsByTagName("ChrStop")[0]) + ")",
                     };
                 };
 
