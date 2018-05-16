ef11560fdb028f653cbf88c82e3158f7a0a3c7fe
Mihir Samdarshi <mihirsamdarshi@yahoo.com>
2018-05-16 16:31:54 -0700

    add eslint ignore

--- web-client/public/js/updateapp.js
+++ web-client/public/js/updateapp.js
@@ -1,3 +1,5 @@
+/* eslint no-unused-vars: [2, {"varsIgnorePattern": "setupHandlers|grnState"}] */
 var setupHandlers = require("./setupHandlers");
 export var updateApp = function (grnState) {
+    return grnState;
 };
