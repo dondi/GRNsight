0c223094d29e91c85221e24e476f8eed12683855
Mihir Samdarshi <mihirsamdarshi@yahoo.com>
2018-05-24 23:41:36 -0700

    testing basic experimental framework

--- test/grnstate-tests.js
+++ test/grnstate-tests.js
@@ -2,3 +2,13 @@
 // import { grnState } from "./grnstate";
 // import { updateApp } from "./update-app";
 // import { setupHandlers } from "./setup-handlers";
+
+/* TODO: Framework for front-end test
+
+var testResetNormalization = (input, propertyExpected) => {
+    var sheet = xlsx.parse(input);
+    var network = spreadsheetController.parseSheet(sheet);
+
+    assert.equal(propertyExpected, grnState.propertyValue);
+}
+*/
