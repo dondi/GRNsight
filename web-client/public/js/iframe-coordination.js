cedcaa39dc5b0cf45710af7ed0c8de804e280d74
dondi <dondi@lmu.edu>
2019-06-24 01:08:28 -0700

    #750 Try to address linting issues.

--- web-client/public/js/iframe-coordination.js
+++ web-client/public/js/iframe-coordination.js
@@ -1,3 +1,7 @@
+/* eslint-disable */
+// ^^^^^ Disabled because this code is meant to be run in the host website and explicitly relies
+//       on top-level names like iFrameResize and jQuery ($).
+
 /**
  * This script is for use by the top-level GRNsight website, in order to coordinate sizing between the
  * host site and the embedded GRNsight application. It is meant to be executed _in the host site_
@@ -7,11 +11,11 @@
  *                loaded into the web browser when this script executes.
  */
 iFrameResize({
-    checkOrigin: ["https://dondi.github.io", "https://grnsight.cs.lmu.edu"],
-    widthCalculationMethod: "taggedElement",
-    heightCalculationMethod: "taggedElement",
-    sizeWidth: true,
-    minWidth: 1081  // Based on minimum width page body and title elements.
+  checkOrigin: ["https://dondi.github.io", "https://grnsight.cs.lmu.edu"],
+  widthCalculationMethod: "taggedElement",
+  heightCalculationMethod: "taggedElement",
+  sizeWidth: true,
+  minWidth: 1081  // Based on minimum width page body and title elements.
 }, "iframe.embedded-demo");
 
 const HEIGHT_OFFSET = 53;
@@ -22,28 +26,28 @@ const MEDIUM_HEIGHT = 840 + HEIGHT_OFFSET;
 const LARGE_HEIGHT = 1080 + HEIGHT_OFFSET;
 
 const sendDimensions = (destination, origin) => {
-    const iframeOffset = $("iframe.embedded-demo").offset();
-    destination.postMessage(
-        {
-            width: $(window).width() - iframeOffset.left,
-            height: $(window).height() - iframeOffset.top
-        },
+  const iframeOffset = $("iframe.embedded-demo").offset();
+  destination.postMessage(
+    {
+      width: $(window).width() - iframeOffset.left,
+      height: $(window).height() - iframeOffset.top
+    },
 
-        origin
-    );
+    origin
+  );
 };
 
 document.querySelector("iframe.embedded-demo").addEventListener("message", event => {
-    if (event.origin.indexOf("https://grnsight.cs.lmu.edu") !== 0) {
-        // Ignore any message that did not originate from the GRNsight web client server.
-        return;
-    }
+  if (event.origin.indexOf("https://grnsight.cs.lmu.edu") !== 0) {
+    // Ignore any message that did not originate from the GRNsight web client server.
+    return;
+  }
 
-    if (event.data === "dimensions") {
-        sendDimensions(event.source, event.origin);
-    }
+  if (event.data === "dimensions") {
+    sendDimensions(event.source, event.origin);
+  }
 });
 
 window.addEventListener("resize", () => sendDimensions(
-    document.querySelector("iframe.embedded-demo").contentWindow, "https://grnsight.cs.lmu.edu"
+  document.querySelector("iframe.embedded-demo").contentWindow, "https://grnsight.cs.lmu.edu"
 ));
