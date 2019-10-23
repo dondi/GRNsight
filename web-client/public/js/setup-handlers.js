31dd1604a4d1e63723b665e6154deeaf4a5be344
Mihir Samdarshi <mihirsamdarshi@yahoo.com>
2019-10-23 14:15:59 -0700

    add traversal code

--- web-client/public/js/setup-handlers.js
+++ web-client/public/js/setup-handlers.js
@@ -77,6 +77,66 @@ export const setupHandlers = grnState => {
         }
     };
 
+// thank you for the help https://github.com/edeno/d3-save-svg/blob/develop/src/setInlineStyles.js
+    const setInlineStyles = (svg) => {
+        // add empty svg element
+        var emptySvg = window.document.createElementNS("http://www.w3.org/2000/svg", "svg");
+        window.document.body.appendChild(emptySvg);
+        var emptySvgDeclarationComputed = window.getComputedStyle(emptySvg);
+
+        const traverse = obj => {
+            var tree = [];
+            tree.push(obj);
+            const visit = (node) => {
+                console.log(node);
+                if (node && node.hasChildNodes()) {
+                    var child = node.firstChild;
+                    while (child) {
+                        if (child.nodeType === 1 && child.nodeName !== "SCRIPT") {
+                            tree.push(child);
+                            visit(child);
+                        }
+                        child = child.nextSibling;
+                    }
+                }
+            };
+            visit(obj);
+            return tree;
+        };
+
+        const explicitlySetStyle = element => {
+            const cSSStyleDeclarationComputed = window.getComputedStyle(element);
+            let i;
+            let len;
+            let key;
+            let value;
+            let computedStyleStr = "";
+
+            for (i = 0, len = cSSStyleDeclarationComputed.length; i < len; i++) {
+                key = cSSStyleDeclarationComputed[i];
+                value = cSSStyleDeclarationComputed.getPropertyValue(key);
+                if (value !== emptySvgDeclarationComputed.getPropertyValue(key)) {
+                    // Don't set computed style of width and height. Makes SVG elmements disappear.
+                    if ((key !== "height") && (key !== "width")) {
+                        computedStyleStr += key + ":" + value + ";";
+                    }
+
+                }
+            }
+
+            element.setAttribute("style", computedStyleStr);
+        };
+
+        // hardcode computed css styles inside svg
+        var allElements = traverse(svg);
+        var i = allElements.length;
+        while (i--) {
+            explicitlySetStyle(allElements[i]);
+        }
+
+        emptySvg.parentNode.removeChild(emptySvg);
+    };
+
     const exportSVG = (svgElement, name) => {
         var serializer = new XMLSerializer();
         var source = serializer.serializeToString(svgElement);
@@ -89,6 +149,7 @@ export const setupHandlers = grnState => {
         }
 
         source = "<?xml version=\"1.0\" standalone=\"no\"?>\r\n" + source;
+        setInlineStyles(source);
         var svgUrl = "data:image/svg+xml;charset=utf-8," + encodeURIComponent(source);
 
         $("#exportAsSvg").attr("href", svgUrl);
