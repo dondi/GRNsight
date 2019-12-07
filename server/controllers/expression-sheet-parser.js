61167045379b26c67095066d2964ecca4b7fec05
Mihir Samdarshi <mihirsamdarshi@yahoo.com>
2019-12-07 14:55:30 -0800

    update to ES6 syntax

--- server/controllers/expression-sheet-parser.js
+++ server/controllers/expression-sheet-parser.js
@@ -1,37 +1,9 @@
 // Parses "optimization_paramters," expression data sheets, and 2-column sheets
 // from GRNmap input or output workbook
 
-var EXPRESSION_SHEET_SUFFIXES = ["_expression", "_optimized_expression", "_sigmas"];
+const EXPRESSION_SHEET_SUFFIXES = ["_expression", "_optimized_expression", "_sigmas"];
 
-var addExpWarning = function (network, message) {
-    var warningsCount;
-    if (!Object.keys(network).includes("warnings")) {
-        warningsCount = 0;
-        network["warnings"] = [];
-    } else {
-        warningsCount = network.warnings.length;
-    }
-    var MAX_WARNINGS = 75;
-    if (warningsCount < MAX_WARNINGS) {
-        network.warnings.push(message);
-    } else {
-        network.errors.push(errorList.warningsCountError);
-        return false;
-    }
-};
-
-var addExpError = function (network, message) {
-    var errorsCount = network.errors.length;
-    var MAX_ERRORS = 20;
-    if (errorsCount < MAX_ERRORS) {
-        network.errors.push(message);
-    } else {
-        network.errors.push(errorList.errorsCountError);
-        return false;
-    }
-};
-
-var errorsList = {
+const errorsList = {
     idLabelError: function () {
         return {
             errorCode: "MISLABELED_ID_CELL",
@@ -55,7 +27,7 @@ var errorsList = {
     },
 };
 
-var warningsList = {
+const warningsList = {
     missingExpressionWarning: function () {
         return {
             warningCode: "MISSING_EXPRESSION_SHEET",
@@ -71,23 +43,51 @@ var warningsList = {
     }
 };
 
-var fillArray = function (value, array, length) { // mutator
+const addExpWarning = (network, message) => {
+    let warningsCount;
+    if (!Object.keys(network).includes("warnings")) {
+        warningsCount = 0;
+        network["warnings"] = [];
+    } else {
+        warningsCount = network.warnings.length;
+    }
+    const MAX_WARNINGS = 75;
+    if (warningsCount < MAX_WARNINGS) {
+        network.warnings.push(message);
+    } else {
+        network.errors.push(errorsList.warningsCountError);
+        return false;
+    }
+};
+
+const addExpError = (network, message) => {
+    const errorsCount = network.errors.length;
+    const MAX_ERRORS = 20;
+    if (errorsCount < MAX_ERRORS) {
+        network.errors.push(message);
+    } else {
+        network.errors.push(errorsList.errorsCountError);
+        return false;
+    }
+};
+
+const fillArray = (value, array, length) => { // mutator
     while (array.length < length) {
         array.push(value);
     }
     return array;
 };
 
-var isExpressionSheet = function (sheetName) {
+const isExpressionSheet = (sheetName) => {
     return EXPRESSION_SHEET_SUFFIXES.some(function (suffix) {
         return sheetName.includes(suffix);
     });
 };
 
 // Going to continue basing this section off of the parseSheet function in spreadsheet-controller.js
-var parseExpressionSheet = function (sheet) {
-    var geneData = {};
-    var expressionData = {
+const parseExpressionSheet = (sheet) => {
+    let geneData = {};
+    let expressionData = {
         genes: [],
         links: [],
         errors: [],
@@ -99,19 +99,19 @@ var parseExpressionSheet = function (sheet) {
     };
 
     // Check that id label is correct. Throw error if not.
-    var idLabel = sheet["data"][0][0];
+    const idLabel = sheet["data"][0][0];
     if (idLabel !== "id") {
         addExpError(expressionData, errorsList.idLabelError());
     }
 
-    expressionData["timePoints"] = sheet.data[0].slice(1);
-    var numberOfDataPoints = expressionData["timePoints"].length;
-    var geneNames = [];
+    expressionData.timePoints = sheet.data[0].slice(1);
+    const numberOfDataPoints = expressionData.timePoints.length;
+    let geneNames = [];
     sheet.data.forEach(function (sheet) {
-        var geneName = sheet[0];
+        const geneName = sheet[0];
         if (geneName) {
             geneNames.push(geneName);
-            var rowData = sheet.slice(1);
+            const rowData = sheet.slice(1);
             // Sometimes, missing data is at the end of the row. In this case, pad the
             // array with nulls
             if (rowData.length < numberOfDataPoints) {
@@ -121,19 +121,19 @@ var parseExpressionSheet = function (sheet) {
         }
     });
     geneNames = geneNames.slice(1);
-    expressionData["data"] = geneData;
-    if (expressionData["data"]["id"]) {
+    expressionData.data = geneData;
+    if (expressionData.data.id) {
         // Throw warning in case of extraneous data
         // Need to add a case where it checks the depth of the columns, as well
-        var rowLength = expressionData["data"]["id"].length;
-        Object.values(expressionData["data"]).forEach(function (row) {
+        const rowLength = expressionData.data.id.length;
+        Object.values(expressionData.data).forEach(function (row) {
             if (row.length !== rowLength) {
                 addExpWarning(expressionData, warningsList.extraneousDataWarning());
             }
 
             // Throw error in case of empty row
-            var nonnullCount = 0;
-            for (var i = 0; i <= rowLength; i++) {
+            let nonnullCount = 0;
+            for (let i = 0; i <= rowLength; i++) {
                 if (i === rowLength) {
                     if (nonnullCount === 0) {
                         addExpError(expressionData, errorsList.emptyExpressionRowError());
@@ -148,8 +148,8 @@ var parseExpressionSheet = function (sheet) {
         });
 
         // Throw error in case of missing column header
-        var nonemptyValues = 0;
-        expressionData["data"]["id"].forEach(function () {
+        let nonemptyValues = 0;
+        expressionData.data.id.forEach(function () {
             nonemptyValues++;
         });
         if (rowLength !== nonemptyValues) {
@@ -162,7 +162,7 @@ var parseExpressionSheet = function (sheet) {
 };
 
 module.exports = function (workbook) {
-    var output = {
+    const output = {
         expression: {}
     };
     workbook.forEach(function (sheet) {
