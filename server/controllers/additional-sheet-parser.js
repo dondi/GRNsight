941146bd6bdd1efbb7561bd051171c4c8a1e48c0
Mihir Samdarshi <mihirsamdarshi@yahoo.com>
2019-12-07 15:05:05 -0800

    es6 changes

--- server/controllers/additional-sheet-parser.js
+++ server/controllers/additional-sheet-parser.js
@@ -1,27 +1,28 @@
 // Parses "optimization_paramters" and 2-column sheets
 // from GRNmap input or output workbook
 
-var TWO_COL_SHEET_NAMES = [
+const TWO_COL_SHEET_NAMES = [
     "production_rates",
     "degradation_rates",
     "threshold_b",
     "optimized_production_rates",
-    "optimized_threshold_b"];
+    "optimized_threshold_b"
+];
 
-var parseMetaDataSheet = function (sheet) {
-    var meta = {};
+const parseMetaDataSheet = (sheet) => {
+    let meta = {};
     sheet.data.forEach(function (element, index) {
         if (index !== 0) {
-            var value = element.slice(1);
+            const value = element.slice(1);
             // Extract element from array if array contains only 1 value
-            meta[element[0]] = value.length > 1 ? value : value[0];
+            meta.element[0] = value.length > 1 ? value : value[0];
         }
     });
     return meta;
 };
 
-var parseTwoColumnSheet = function (sheet) {
-    var data = {};
+const parseTwoColumnSheet = (sheet) => {
+    let data = {};
     sheet.data.forEach(function (element, index) {
         if (index !== 0) {
             data[element[0]] = element[1];
@@ -31,17 +32,17 @@ var parseTwoColumnSheet = function (sheet) {
 };
 
 module.exports = function (workbook) {
-    var output = {
+    let output = {
         meta: {},
         test: {} // 2-column data
     };
     workbook.forEach(function (sheet) {
         if (sheet.name === "optimization_parameters") {
-            output["meta"] = parseMetaDataSheet(sheet);
+            output.meta = parseMetaDataSheet(sheet);
         // Parse 2-column sheets
         } else if (TWO_COL_SHEET_NAMES.includes(sheet.name)) {
             output["test"][sheet.name] = parseTwoColumnSheet(sheet);
-        } 
+        }
     });
     return output;
 };
