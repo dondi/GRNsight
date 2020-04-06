58cee80d6b43a72d03a9ba4c889e02841dce12ba
Alexia Filler <afiller1@lion.lmu.edu>
2020-04-05 19:42:24 -0700

    Got query to workgit add server/controllers/database-controller.js ! Need to find solution for committing password. Also need to check package and package-lock bc I installed sequelize package.

--- server/controllers/database-controller.js
+++ server/controllers/database-controller.js
@@ -1,7 +1,19 @@
+var Sequelize = require("sequelize");
+
+var sequelize = new Sequelize('grnsight_database', 'grnsight', "PASSWORD", {
+    host: 'localhost',
+    dialect: 'postgres',
+  
+    pool: {
+      max: 5,
+      min: 0,
+      idle: 10000
+    },
+  
+  });
 
 module.exports = function(app) {
 // respond with "hello world" when a GET request is made to the homepage
-console.log("getting here?");
     app.get('/hello-world', function (req, res) {
         try {
            return res.send('hello world')
@@ -10,6 +22,16 @@ console.log("getting here?");
         }
 
 
+    })
+
+    app.get('/first-query', function (req, res) {
+        try {
+            return sequelize.query("SELECT standard_name FROM degradationrates", { type: sequelize.QueryTypes.SELECT }).then(function(stdname) {
+                return res.send(stdname);
+            });
+        } catch (e) {
+            res.json({error: e.message});
+        }
     })
 
 }
