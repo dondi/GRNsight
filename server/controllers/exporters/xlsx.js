41433502aafefba34d4a068c2712189891c8224d
Onariaginosa <ona.igbinedion@gmail.com>
2021-10-21 14:26:16 -0700

    testing expression sheet export issue

--- server/controllers/exporters/xlsx.js
+++ server/controllers/exporters/xlsx.js
@@ -76,9 +76,20 @@ const buildMetaSheet = function (metaDataContainer) {
     return meta;
 };
 
+const EXPRESSION_SHEET_SUFFIXES = ["_expression", "_optimized_expression", "_sigmas"];
+
+const isExpressionSheet = (sheetName) => {
+    return EXPRESSION_SHEET_SUFFIXES.some(function (suffix) {
+        return sheetName.includes(suffix);
+    });
+};
+
 const buildExpressionSheets = function (expressions) {
     const builtExpressionSheets = [];
     Object.keys(expressions).forEach((expression) => {
+        if (!isExpressionSheet(expression)) {
+            expression = expression + "_expression";
+        }
         const builtSheet = { name: expression, data: []};
         Object.keys(expressions[expression]["data"]).forEach((key) => {
             const expressionData = expressions[expression]["data"][key];
