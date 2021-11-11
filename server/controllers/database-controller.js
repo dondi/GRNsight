48e99234fbaa07bcf87c8d898159c6020adb2b84
Onariaginosa <ona.igbinedion@gmail.com>
2021-11-10 20:26:58 -0800

    changed the query a bit to encapsulate the new naming structure for the internal gene_id in the expression table

--- server/controllers/database-controller.js
+++ server/controllers/database-controller.js
@@ -64,23 +64,23 @@ timepointsSources.forEach(source => timepointsByDataset[source.key] = source.val
 
 let buildTimepointsQuery = function (selection) {
     let timepoints = "";
-    selection.forEach(x => timepoints += ("timepoints=\'" + x + "\' OR "));
+    selection.forEach(x => timepoints += ("fall2021.expression.timepoint=" + x + " OR "));
     return timepoints.substring(0, timepoints.length - 4);
 };
 
 let buildGenesQuery = function (geneString) {
     let genes = "";
     let geneList = geneString.split(",");
-    geneList.forEach(x => genes += ("standardname=\'" + x + "\' OR "));
+    geneList.forEach(x => genes += ("fall2021.gene.display_gene_id=\'" + x + "\' OR "));
     return genes.substring(0, genes.length - 4);
 };
 
 let buildQuery = function (dataset, timepoints, genes) {
     return timepoints ?
-    `SELECT * FROM expressiondata WHERE dataset='${dataset}' AND
+    `SELECT DISTINCT *  FROM fall2021.expression, fall2021.gene WHERE fall2021.expression.dataset='${dataset}' AND
     (${buildTimepointsQuery(timepoints)}) ORDER BY sortindex AND
     (${buildGenesQuery(genes)}) ORDER BY sortindex;`
-    : `SELECT * FROM expressiondata WHERE dataset='${dataset}'
+    : `SELECT DISTINCT * FROM fall2021.expression WHERE fall2021.expression.dataset='${dataset}'
     AND (${buildGenesQuery(genes)}) ORDER BY sortindex;`;
 };
 
