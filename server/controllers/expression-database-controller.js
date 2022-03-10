d6e22e654406a4b80c62de8d22a49cef76565830
Onariaginosa <ona.igbinedion@gmail.com>
2022-03-10 15:05:04 -0800

    changed the import for the expression dal

--- server/controllers/expression-database-controller.js
+++ server/controllers/expression-database-controller.js
@@ -1,11 +1,12 @@
 
-import queryExpressionDatabase from "../dals/expression-dal";
+var expression_dal = require(__dirname + "/../dals/expression-dal")
+
 
 module.exports = function (app) {
 
     app.get("/expressiondb", function (req, res) {
         try {
-            return queryExpressionDatabase(req, res);
+            return expression_dal.queryExpressionDatabase(req, res);
         } catch (e) {
             res.json({error: e.stack});
             res.json({error: e.name});
