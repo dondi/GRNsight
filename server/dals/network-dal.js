146c92ef611b5bf652296376147247520d042157
Onariaginosa <ona.igbinedion@gmail.com>
2022-04-11 08:34:21 -0700

    fixed request again. Still testing

--- server/dals/network-dal.js
+++ server/dals/network-dal.js
@@ -20,14 +20,27 @@ var sequelize = new Sequelize(
 const buildNetworkSourceQuery = function () {
     return "SELECT * FROM spring2022_network.source ORDER BY time_stamp";
 };
+const buildQueryByType = function (queryType) {
+    switch (queryType) {
+    case "NetworkSource":
+        console.log("building source :)");
+        return buildNetworkSourceQuery();
+    case "":
+        // code block
+        console.log("what happened :((");
+        break;
+    }
+};
 
 let convertResponseToJSON = function (queryType, totalOutput) {
     switch (queryType) {
     case "NetworkSource":
+        console.log("got response :)");
         // let JSONOutput = {}
         return totalOutput;
     case "":
         // code block
+        console.log("what happened :(");
         break;
     }
 };
@@ -35,7 +48,7 @@ let convertResponseToJSON = function (queryType, totalOutput) {
 module.exports = {
     buildNetworkSourceQuery: buildNetworkSourceQuery,
     queryNetworkDatabase: function (req, res) {
-        return sequelize.query(req.query.query, { type: sequelize.QueryTypes.SELECT })
+        return sequelize.query(buildQueryByType(req.query.type), { type: sequelize.QueryTypes.SELECT })
             .then(function (stdname) {
                 let response = convertResponseToJSON(req.query.type, stdname);
                 return res.send(response);
