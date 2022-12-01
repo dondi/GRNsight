ba31ffbc58dd5aa13dc1563fa5ec76730edcf558
Sarronnn <stadesse@lion.lmu.edu>
2022-12-01 12:02:35 -0800

    Moved functions into grnsight-api.js

--- web-client/public/js/api/grnsight-api.js
+++ web-client/public/js/api/grnsight-api.js
@@ -1,43 +1,172 @@
+import { responseCustomWorkbookData } from "../setup-load-and-import-handlers";
+
 // Expression DB Access Functions
-const buildExpressionTimepointsString = function (selection) {
-    let timepoints = "";
-    selection.timepoints.forEach(x => timepoints += (x + ","));
-    return timepoints.substring(0, timepoints.length - 1);
+const buildExpressionTimepointsString = function(selection) {
+  let timepoints = "";
+  selection.timepoints.forEach((x) => (timepoints += x + ","));
+  return timepoints.substring(0, timepoints.length - 1);
 };
-const buildExpressionGeneQuery = function (workbookGenes) {
-    let genes = "";
-    workbookGenes.forEach(x => genes += (x.name + ","));
-    return genes.substring(0, genes.length - 1);
+const buildExpressionGeneQuery = function(workbookGenes) {
+  let genes = "";
+  workbookGenes.forEach((x) => (genes += x.name + ","));
+  return genes.substring(0, genes.length - 1);
 };
 
-const buildExpressionURL = function (selection, genes) {
-    const baseQuery = `expressiondb?dataset=${selection.dataset}&genes=${buildExpressionGeneQuery(genes)}`;
-    return selection.timepoints ?
-    `${baseQuery}&timepoints=${buildExpressionTimepointsString(selection)}` :
-    baseQuery;
+const buildExpressionURL = function(selection, genes) {
+  const baseQuery = `expressiondb?dataset=${
+    selection.dataset
+  }&genes=${buildExpressionGeneQuery(genes)}`;
+  return selection.timepoints
+    ? `${baseQuery}&timepoints=${buildExpressionTimepointsString(selection)}`
+    : baseQuery;
 };
 
 const responseExpressionData = (formData, queryURL) => {
-    return new Promise(function (resolve) {
-        const uploadRoute = queryURL;
-        const fullUrl = [ $("#service-root").val(), uploadRoute ].join("/");
-        (formData ?
-            $.ajax({
-                url: fullUrl,
-                data: formData,
-                processData: false,
-                contentType: false,
-                type: "GET",
-                crossDomain: true
-            }) :
-            $.getJSON(fullUrl)
-            ).done((expressionData) => {
-                resolve(expressionData);
-            }).error(console.log("Error in accessing expression database. Result may just be loading."));
-    });
+  return new Promise(function(resolve) {
+    const uploadRoute = queryURL;
+    const fullUrl = [$(".service-root").val(), uploadRoute].join("/");
+    (formData
+      ? $.ajax({
+          url: fullUrl,
+          data: formData,
+          processData: false,
+          contentType: false,
+          type: "GET",
+          crossDomain: true,
+        })
+      : $.getJSON(fullUrl)
+    )
+      .done((expressionData) => {
+        resolve(expressionData);
+      })
+      .error(
+        console.log(
+          "Error in accessing expression database. Result may just be loading."
+        )
+      );
+  });
 };
 
-export const queryExpressionDatabase = (query) => {
-    let queryURL = buildExpressionURL({dataset: query.dataset}, query.genes);
-    return responseExpressionData("", queryURL);
+const queryExpressionDatabase = (query) => {
+  let queryURL = buildExpressionURL({ dataset: query.dataset }, query.genes);
+  return responseExpressionData("", queryURL);
+};
+
+// Network DB Access Functions
+
+const buildNetworkGenesQuery = (genes) => {
+  let result = "";
+  for (let gene in genes) {
+    result += `${gene},`;
+  }
+  return result.substring(0, result.length - 1);
+};
+
+const buildNetworkURL = function(queryType, queryInfo) {
+  let baseQuery = `networkdb?type=${queryType}`;
+  if (queryInfo !== null) {
+    for (let header in queryInfo) {
+      if (header === "genes") {
+        baseQuery += `&${header}=${buildNetworkGenesQuery(queryInfo[header])}`;
+      } else {
+        baseQuery += `&${header}=${queryInfo[header]}`;
+      }
+    }
+  }
+  return baseQuery;
+};
+
+const responseNetworkData = (formData, queryURL) => {
+  return new Promise(function(resolve) {
+    const uploadRoute = queryURL;
+    const fullUrl = [$(".service-root").val(), uploadRoute].join("/");
+    (formData
+      ? $.ajax({
+          url: fullUrl,
+          data: formData,
+          processData: false,
+          contentType: false,
+          type: "GET",
+          crossDomain: true,
+        })
+      : $.getJSON(fullUrl)
+    )
+      .done((networkData) => {
+        resolve(networkData);
+      })
+      .error(
+        console.log(
+          "Error in accessing network database. Result may just be loading."
+        )
+      );
+  });
+};
+
+const queryNetworkDatabase = (query) => {
+  let queryURL = buildNetworkURL(query.type, query.info);
+  return responseNetworkData("", queryURL);
+};
+
+// Upload Custom Workbook Functions
+const buildCustomWorkbookURL = (name, genes, links) => {
+  let baseQuery = `upload-custom-workbook?name=${name}`;
+  let genesString = "";
+  let linksString = "";
+  let genesByIndex = {};
+  let i = 0;
+  for (let gene in genes) {
+    genesString += `${genes[gene]},`;
+    genesByIndex[gene] = i;
+    i++;
+  }
+  for (let regulator in links) {
+    for (let target of links[regulator]) {
+      linksString += `${genesByIndex[regulator]}->${genesByIndex[target]},`;
+    }
+  }
+  baseQuery += `&genes=${genesString.substring(0, genesString.length - 1)}`;
+  baseQuery += `&links=${linksString.substring(0, linksString.length - 1)}`;
+  return baseQuery;
+};
+
+const uploadCustomWorkbook = (workbook, grnState) => {
+  let queryURL = buildCustomWorkbookURL(
+    workbook.name,
+    workbook.genes,
+    workbook.links
+  );
+
+  return responseCustomWorkbookData(grnState, queryURL, workbook.name);
+};
+
+const constructFullUrl = (queryURL) =>
+  [$(".service-root").val(), queryURL].join("/");
+
+const getWorkbookFromForm = (formData, queryURL) => {
+  const fullUrl = constructFullUrl(queryURL);
+
+  // The presence of formData is taken to indicate a POST.
+  return formData
+    ? $.ajax({
+        url: fullUrl,
+        data: formData,
+        processData: false,
+        contentType: false,
+        type: "POST",
+        crossDomain: true,
+      })
+    : $.getJSON(fullUrl);
+};
+
+const getWorkbookFromUrl = (queryURL) => {
+  const fullUrl = constructFullUrl(queryURL);
+  return $.getJSON(fullUrl);
+};
+
+export {
+  queryExpressionDatabase,
+  queryNetworkDatabase,
+  uploadCustomWorkbook,
+  getWorkbookFromForm,
+  getWorkbookFromUrl,
 };
