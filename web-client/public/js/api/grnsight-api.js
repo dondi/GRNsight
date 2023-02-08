e6c40d5d612368ce6827c6500a9803b884a95f49
Onariaginosa <ona.igbinedion@gmail.com>
2023-02-08 11:06:53 -0800

    #1018 mostly completed and fixed pre-existing linter errors from when travis was down

--- web-client/public/js/api/grnsight-api.js
+++ web-client/public/js/api/grnsight-api.js
@@ -1,88 +1,89 @@
 import { responseCustomWorkbookData } from "../setup-load-and-import-handlers";
 // General DB Access Functions
-const buildQueryURL = function(path, parameters) {
-  const searchParams = new URLSearchParams("");
-  for (let p in parameters) {
-    searchParams.append(p, parameters[p]);
-  }
-  return `${path}?${searchParams.toString()}`;
+const buildQueryURL = function (path, parameters) {
+    const searchParams = new URLSearchParams("");
+    for (let p in parameters) {
+        searchParams.append(p, parameters[p]);
+    }
+    return `${path}?${searchParams.toString()}`;
 };
 
 const responseData = (database, formData, queryURL) => {
-  return new Promise(function(resolve) {
-    const uploadRoute = queryURL;
-    const fullUrl = [$(".service-root").val(), uploadRoute].join("/");
-    (formData
-      ? $.ajax({
-          url: fullUrl,
-          data: formData,
-          processData: false,
-          contentType: false,
-          type: "GET",
-          crossDomain: true,
-        })
-      : $.getJSON(fullUrl)
-    )
-      .done((data) => {
-        resolve(data);
-      })
-      .error(function() {
-        console.log(
-          `Error in accessing ${database} database. Result may just be loading.`
-        );
-      });
-  });
+    return new Promise(function (resolve) {
+        const uploadRoute = queryURL;
+        const fullUrl = [$(".service-root").val(), uploadRoute].join("/");
+        (formData
+            ? $.ajax({
+                url: fullUrl,
+                data: formData,
+                processData: false,
+                contentType: false,
+                type: "GET",
+                crossDomain: true,
+            })
+            : $.getJSON(fullUrl)
+        )
+            .done((data) => {
+                resolve(data);
+            })
+            .error(function () {
+                console.log(
+                    `Error in accessing ${database} database. Result may just be loading.`
+                );
+            });
+    });
 };
 
 // Expression DB Access Functions
 
 const queryExpressionDatabase = (query) => {
-  const queryURL = buildQueryURL("expressiondb", query);
-  return responseData("expression", "", queryURL);
+    const queryURL = buildQueryURL("expressiondb", query);
+    return responseData("expression", "", queryURL);
 };
 
 // Network DB Access Functions
 
 const queryNetworkDatabase = (query) => {
-  const queryURL = buildQueryURL("networkdb", query);
-  return responseData("network", "", queryURL);
+    const queryURL = buildQueryURL("networkdb", query);
+    return responseData("network", "", queryURL);
 };
 
 // Upload Custom Workbook Functions
 
 const uploadCustomWorkbook = (workbook, grnState) => {
-  const queryURL = buildQueryURL("upload-custom-workbook", workbook);
-  return responseCustomWorkbookData(grnState, queryURL, workbook.name);
+    const queryURL = buildQueryURL("upload-custom-workbook", workbook);
+    return responseCustomWorkbookData(grnState, queryURL, workbook.name);
 };
 
 const constructFullUrl = (queryURL) =>
-  [$(".service-root").val(), queryURL].join("/");
+    [$(".service-root").val(), queryURL].join("/");
 
 const getWorkbookFromForm = (formData, queryURL) => {
-  const fullUrl = constructFullUrl(queryURL);
+    const fullUrl = constructFullUrl(queryURL);
 
-  // The presence of formData is taken to indicate a POST.
-  return formData
-    ? $.ajax({
-        url: fullUrl,
-        data: formData,
-        processData: false,
-        contentType: false,
-        type: "POST",
-        crossDomain: true,
-      })
-    : $.getJSON(fullUrl);
+
+    // The presence of formData is taken to indicate a POST.
+    return formData
+        ? $.ajax({
+            url: fullUrl,
+            data: formData,
+            processData: false,
+            contentType: false,
+            type: "POST",
+            crossDomain: true,
+        })
+        : $.getJSON(fullUrl);
 };
 
 const getWorkbookFromUrl = (queryURL) => {
-  const fullUrl = constructFullUrl(queryURL);
-  return $.getJSON(fullUrl);
+    const fullUrl = constructFullUrl(queryURL);
+    return $.getJSON(fullUrl);
 };
 
 export {
-  queryExpressionDatabase,
-  queryNetworkDatabase,
-  uploadCustomWorkbook,
-  getWorkbookFromForm,
-  getWorkbookFromUrl,
+    queryExpressionDatabase,
+    queryNetworkDatabase,
+    uploadCustomWorkbook,
+    getWorkbookFromForm,
+    getWorkbookFromUrl,
 };
