2e2a938140d79be7efb35a57d55bc014a40d628e
ntran18 <108908370+ntran18@users.noreply.github.com>
2023-09-07 19:25:24 -0700

    Fixing bug #1033

--- web-client/public/js/setup-handlers.js
+++ web-client/public/js/setup-handlers.js
@@ -239,8 +239,8 @@ export const setupHandlers = grnState => {
         updateApp(grnState);
     });
 
-    $(TOP_DATASET_SELECTION_MENU).click(() => {
-        var selection = $(this).attr("value");
+    $(TOP_DATASET_SELECTION_MENU).click((event) => {
+        const selection = event.target.textContent.trim();
         grnState.nodeColoring.topDataset = selection;
         if (grnState.nodeColoring.bottomDataSameAsTop) {
             grnState.nodeColoring.bottomDataset = selection;
