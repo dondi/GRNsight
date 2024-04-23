8652b3308fc28d9f611a178d6169483a21d0bf89
ntran18 <108908370+ntran18@users.noreply.github.com>
2024-04-23 11:38:02 -0700

    Changing the gene source directory to use the old gene files, instead of using union genes. The code for union genes can just leave it there for now

--- database/loader_update.py
+++ database/loader_update.py
@@ -13,17 +13,14 @@ Utils.create_union_file([Constants.PROTEIN_UPDATE_GENE_DIRECTORY, Constants.NETW
 
 # Proteins
 Utils.load_sources(Constants.PROTEIN_SOURCE_TABLE_DATA_DIRECTORY, Constants.PROTEIN_DATABASE_NAMESPACE)
-Utils.update_protein_genes(Constants.UPDATE_GENE_UNION_DIRECTORY, Constants.PROTEIN_DATABASE_NAMESPACE)
+Utils.update_protein_genes(Constants.PROTEIN_UPDATE_GENE_DIRECTORY, Constants.PROTEIN_DATABASE_NAMESPACE)
 Utils.update_proteins(Constants.PROTEIN_UPDATE_PROTEIN_DIRECTORY, Constants.PROTEIN_DATABASE_NAMESPACE)
-Utils.load_genes(Constants.MISSING_GENE_UNION_DIRECTORY, Constants.PROTEIN_DATABASE_NAMESPACE, is_protein=True)
+Utils.load_protein_genes(Constants.PROTEIN_MISSING_GENE_DIRECTORY, Constants.PROTEIN_DATABASE_NAMESPACE)
 Utils.load_proteins(Constants.PROTEIN_MISSING_PROTEIN_DIRECTORY, Constants.PROTEIN_DATABASE_NAMESPACE)
-Utils.load_network(Constants.PROTEIN_NETWORK_TABLE_DATA_DIRECTORY, Constants.PROTEIN_DATABASE_NAMESPACE, is_protein=True)
+Utils.load_protein_network(Constants.PROTEIN_NETWORK_TABLE_DATA_DIRECTORY, Constants.PROTEIN_DATABASE_NAMESPACE)
 
 # Network
 Utils.load_sources(Constants.NETWORK_SOURCE_TABLE_DATA_DIRECTORY, Constants.NETWORK_DATABASE_NAMESPACE)
-Utils.update_network_genes(Constants.UPDATE_GENE_UNION_DIRECTORY, Constants.NETWORK_DATABASE_NAMESPACE)
-Utils.load_network_genes(Constants.MISSING_GENE_UNION_DIRECTORY, Constants.NETWORK_DATABASE_NAMESPACE)
+Utils.update_network_genes(Constants.NETWORK_UPDATE_GENE_DIRECTORY, Constants.NETWORK_DATABASE_NAMESPACE)
+Utils.load_network_genes(Constants.NETWORK_MISSING_GENE_DIRECTORY, Constants.NETWORK_DATABASE_NAMESPACE)
 Utils.load_network_network(Constants.NETWORK_NETWORK_TABLE_DATA_DIRECTORY, Constants.NETWORK_DATABASE_NAMESPACE)
-
-# Expression
-Utils.update_expression_genes(Constants.UPDATE_GENE_UNION_DIRECTORY, Constants.EXPRESISON_DATABASE_NAMESPACE)
