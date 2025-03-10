69a47d6672a70addf64d356b7af21f402d28451a
ceciliazaragoza <czarago3@lion.lmu.edu>
2025-03-10 15:56:54 -0700

    changing namespace from gene_regulatory_network_new to gene_regulatory_network_with_timestamp

--- server/dals/dbConstants.js
+++ server/dals/dbConstants.js
@@ -1,6 +1,6 @@
-export const GRN_DATABASE_NAMESPACE = "gene_regulatory_network_new";
+export const GRN_DATABASE_NAMESPACE = "gene_regulatory_network_with_timestamp";
 export const GRN_DATABASE_NAMESPACE_OLD = "gene_regulatory_network";
-export const PPI_DATABASE_NAMESPACE = "protein_protein_interactions_new";
+export const PPI_DATABASE_NAMESPACE = "protein_protein_interactions_with_timestamp";
 export const PPI_DATABASE_NAMESPACE_OLD = "protein_protein_interactions";
 export const DATABASE_TIMESTAMP_CUTOFF = new Date("2025-01-01");
 
