618657f94931762a44df87fe55344bd3cb88a52b
ntran18 <108908370+ntran18@users.noreply.github.com>
2025-03-10 15:55:51 -0700

    Change the new namespace for GRN and PPI from new to with_timestamp

--- database/schema/protein_protein_interactions_schema.sql
+++ database/schema/protein_protein_interactions_schema.sql
@@ -1,13 +1,13 @@
-CREATE SCHEMA protein_protein_interactions_new;
+CREATE SCHEMA protein_protein_interactions_with_timestamp;
 
-CREATE TABLE protein_protein_interactions_new.source (
+CREATE TABLE protein_protein_interactions_with_timestamp.source (
   time_stamp TIMESTAMP WITH TIME ZONE,
   source VARCHAR,
   display_name VARCHAR,
   PRIMARY KEY(time_stamp, source)
 );
 
-CREATE TABLE protein_protein_interactions_new.gene (
+CREATE TABLE protein_protein_interactions_with_timestamp.gene (
   gene_id VARCHAR, -- systematic like name
   display_gene_id VARCHAR, -- standard like name
   species VARCHAR,
@@ -15,10 +15,10 @@ CREATE TABLE protein_protein_interactions_new.gene (
   time_stamp TIMESTAMP WITH TIME ZONE,
   source VARCHAR,
   PRIMARY KEY(gene_id, taxon_id),
-  FOREIGN KEY (time_stamp, source) REFERENCES protein_protein_interactions_new.source(time_stamp, source)
+  FOREIGN KEY (time_stamp, source) REFERENCES protein_protein_interactions_with_timestamp.source(time_stamp, source)
 ); 
 
-CREATE TABLE protein_protein_interactions_new.protein (
+CREATE TABLE protein_protein_interactions_with_timestamp.protein (
   standard_name VARCHAR PRIMARY KEY,
   gene_systematic_name VARCHAR,
   length FLOAT,
@@ -27,11 +27,11 @@ CREATE TABLE protein_protein_interactions_new.protein (
   taxon_id VARCHAR,
   time_stamp TIMESTAMP WITH TIME ZONE,
   source VARCHAR,
-  FOREIGN KEY (gene_systematic_name, taxon_id) REFERENCES protein_protein_interactions_new.gene(gene_id, taxon_id),
-  FOREIGN KEY (time_stamp, source) REFERENCES protein_protein_interactions_new.source(time_stamp, source)
+  FOREIGN KEY (gene_systematic_name, taxon_id) REFERENCES protein_protein_interactions_with_timestamp.gene(gene_id, taxon_id),
+  FOREIGN KEY (time_stamp, source) REFERENCES protein_protein_interactions_with_timestamp.source(time_stamp, source)
 );
 
-  CREATE TABLE protein_protein_interactions_new.physical_interactions (
+  CREATE TABLE protein_protein_interactions_with_timestamp.physical_interactions (
     protein1 VARCHAR,
     protein2 VARCHAR,
     gene_systematic_name1 VARCHAR,
@@ -41,8 +41,8 @@ CREATE TABLE protein_protein_interactions_new.protein (
     experiment_name VARCHAR,
     time_stamp TIMESTAMP WITH TIME ZONE,
     source VARCHAR,
-    FOREIGN KEY (protein1) REFERENCES protein_protein_interactions_new.protein(standard_name),
-    FOREIGN KEY (protein2) REFERENCES protein_protein_interactions_new.protein(standard_name),
-    FOREIGN KEY (time_stamp, source) REFERENCES protein_protein_interactions_new.source(time_stamp, source),
+    FOREIGN KEY (protein1) REFERENCES protein_protein_interactions_with_timestamp.protein(standard_name),
+    FOREIGN KEY (protein2) REFERENCES protein_protein_interactions_with_timestamp.protein(standard_name),
+    FOREIGN KEY (time_stamp, source) REFERENCES protein_protein_interactions_with_timestamp.source(time_stamp, source),
     CONSTRAINT unique_physical_interaction UNIQUE (protein1, protein2, interaction_detection_methods_identifier, annotation_type, experiment_name, time_stamp, source)
   );
