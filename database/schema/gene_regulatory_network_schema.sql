ae95bba18b0d08df35bcf17e02b2baee34cae56d
ntran18 <108908370+ntran18@users.noreply.github.com>
2025-03-17 15:46:33 -0700

    #1120 Adding back the old schemas for gene regulatory network and protein protein interactions

--- database/schema/gene_regulatory_network_schema.sql
+++ database/schema/gene_regulatory_network_schema.sql
@@ -1,33 +1,27 @@
-CREATE SCHEMA gene_regulatory_network_with_timestamp;
+CREATE SCHEMA gene_regulatory_network;
 
-CREATE TABLE gene_regulatory_network_with_timestamp.source (
+CREATE TABLE gene_regulatory_network.source (
   time_stamp TIMESTAMP WITH TIME ZONE,
   source VARCHAR,
   display_name VARCHAR,
   PRIMARY KEY(time_stamp, source)
 );
 
-CREATE TABLE gene_regulatory_network_with_timestamp.gene (
+CREATE TABLE gene_regulatory_network.gene (
   gene_id VARCHAR, -- systematic like name
   display_gene_id VARCHAR, -- standard like name
   species VARCHAR,
   taxon_id VARCHAR,
   regulator BOOLEAN,
-  time_stamp TIMESTAMP WITH TIME ZONE,
-  source VARCHAR,
-  PRIMARY KEY(gene_id, taxon_id),
-  FOREIGN KEY (time_stamp, source) REFERENCES gene_regulatory_network_with_timestamp.source(time_stamp, source)
-);
-
-CREATE TABLE gene_regulatory_network_with_timestamp.network (
+  PRIMARY KEY(gene_id, taxon_id)
+); 
+CREATE TABLE gene_regulatory_network.network (
   regulator_gene_id VARCHAR,
   target_gene_id VARCHAR,
   taxon_id VARCHAR,
-  annotation_type VARCHAR,
   time_stamp TIMESTAMP WITH TIME ZONE,
   source VARCHAR,
-  FOREIGN KEY (regulator_gene_id, taxon_id) REFERENCES gene_regulatory_network_with_timestamp.gene(gene_id, taxon_id),
-  FOREIGN KEY (target_gene_id, taxon_id) REFERENCES gene_regulatory_network_with_timestamp.gene(gene_id, taxon_id),
-  FOREIGN KEY (time_stamp, source) REFERENCES gene_regulatory_network_with_timestamp.source(time_stamp, source),
-  CONSTRAINT unique_network UNIQUE (regulator_gene_id, target_gene_id, taxon_id, time_stamp, source, annotation_type)
+  FOREIGN KEY (regulator_gene_id, taxon_id) REFERENCES gene_regulatory_network.gene(gene_id, taxon_id),
+  FOREIGN KEY (target_gene_id, taxon_id) REFERENCES gene_regulatory_network.gene(gene_id, taxon_id),
+  FOREIGN KEY (time_stamp, source) REFERENCES gene_regulatory_network.source(time_stamp, source)
 ); 
