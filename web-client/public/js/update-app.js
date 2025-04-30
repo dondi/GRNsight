25f4353115a42c336495c9a5a456788f30127e4c
Amelie1253 <adinh2@lion.lmu.edu>
2025-04-30 05:18:19 -0700

    cleaned up code

--- web-client/public/js/update-app.js
+++ web-client/public/js/update-app.js
@@ -742,18 +742,15 @@ const resetDatasetDropdownMenus = (workbook) => {
 
     // Add Workbook Expressions
     if (grnState.nodeColoring.nodeColoringOptions.workbookExpressions.length > 0) {
-    
         if (grnState.workbookType === "upload") {
             addOptionsToDropdown(grnState.nodeColoring.nodeColoringOptions.workbookExpressions, "User-Uploaded");
         } else {
             addOptionsToDropdown(grnState.nodeColoring.nodeColoringOptions.workbookExpressions, "Demo");
         }
     }
-    
     // Always add database expressions
     addOptionsToDropdown(grnState.nodeColoring.nodeColoringOptions.databaseExpressions, "Expression Database");
     
-
     $("#topDatasetDropdownMenu li a span").first().addClass("glyphicon-ok");
     $("#bottomDatasetDropdownMenu li a span").first().addClass("glyphicon-ok");
 };
