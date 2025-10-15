d45e322bed2c06c242f92db15337af658292e008
ceciliazaragoza <czarago3@lion.lmu.edu>
2025-10-15 11:54:34 -0700

    updating vite-config to ensure /GRNsight/ included in beginning of URL

--- web-client/vite.config.js
+++ web-client/vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: "/react-thesis-4081/",
+    base: "/GRNsight/react-thesis-4081/",
 });
