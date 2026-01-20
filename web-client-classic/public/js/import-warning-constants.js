7f923379694b7395f0dcb947aa882b2b60e88f06
ntran18 <108908370+ntran18@users.noreply.github.com>
2026-01-19 21:44:53 -0800

    1192 Adding missing space in the import warning message

--- web-client-classic/public/js/import-warning-constants.js
+++ web-client-classic/public/js/import-warning-constants.js
@@ -11,7 +11,7 @@ module.exports = {
         MISSING_PRODUCTION_RATES: (missingGenes = "") => ({
             warningCode: "MISSING_PRODUCTION_RATES_IMPORT_WARNING",
             errorDescription:
-                "GRNsight has detected that the imported workbook does not have a production rate in the production_rates sheet." +
+                "GRNsight has detected that the imported workbook does not have a production rate in the production_rates sheet. " +
                 "A production rate will need to be supplied to run this workbook in GRNmap, but will not affect the display of the graph in GRNsight",
         }),
     },
