b458db6b361ce228afac2cf7ab8b580d87f68a6f
ntran18 <108908370+ntran18@users.noreply.github.com>
2026-01-26 23:48:13 -0800

    #1192 Fix the import warning messages to display the actual missing genes

--- web-client-classic/public/js/import-warning-constants.js
+++ web-client-classic/public/js/import-warning-constants.js
@@ -5,14 +5,18 @@ module.exports = {
             warningCode: "MISSING_DEGRADATION_RATES_IMPORT_WARNING",
             errorDescription:
                 "GRNsight has detected that the imported workbook does not have a degradation rate in the degradation_rates sheet. " +
-                "A degradation rate will need to be supplied to run this workbook in GRNmap, but will not affect the display of the graph in GRNsight",
+                "A degradation rate will need to be supplied to run this workbook in GRNmap, but will not affect the display of the graph in GRNsight. " +
+                "The missing values are for the genes: " +
+                missingGenes,
         }),
 
         MISSING_PRODUCTION_RATES: (missingGenes = "") => ({
             warningCode: "MISSING_PRODUCTION_RATES_IMPORT_WARNING",
             errorDescription:
                 "GRNsight has detected that the imported workbook does not have a production rate in the production_rates sheet. " +
-                "A production rate will need to be supplied to run this workbook in GRNmap, but will not affect the display of the graph in GRNsight",
+                "A production rate will need to be supplied to run this workbook in GRNmap, but will not affect the display of the graph in GRNsight. " +
+                "The missing values are for the genes: " +
+                missingGenes,
         }),
     },
 };
