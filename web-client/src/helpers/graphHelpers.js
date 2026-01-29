983ffa7718549bafbb75d6b9c5f3ea48da6bafa2
ceciliazaragoza <czarago3@lion.lmu.edu>
2026-01-29 07:34:47 -0800

    removing white space

--- web-client/src/helpers/graphHelpers.js
+++ web-client/src/helpers/graphHelpers.js
@@ -400,5 +400,3 @@ export function calcAllWeights(data, colorOptimal) {
 export function calcMaxWeight(allWeights) {
   return Math.max(Math.abs(d3.max(allWeights)), Math.abs(d3.min(allWeights)));
 }
-
-
