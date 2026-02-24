42ed7974eff591904bb63e0d55fabfb139bd643c
MilkaZek <143560370+MilkaZek@users.noreply.github.com>
2026-02-24 01:41:39 -0800

    For #1296, cleaned up error description syntax and wording

--- server/controllers/export-constants.js
+++ server/controllers/export-constants.js
@@ -45,8 +45,8 @@ module.exports = {
                 "sheet is improperly formattedPlease look over your exported workbook and ensure that network",
                 `sheets have cell A1 as '${CELL_A1_GRN}' or '${CELL_A1_PPI}' exactly`,
                 "and all gene are using the  naming conventions listed at:",
-                " \nhttps://dondi.github.io/GRNsight/documentation.html."
-            ].join(' '),
+                " \nhttps://dondi.github.io/GRNsight/documentation.html.",
+            ].join(" "),
         },
         // Missing Source Genes
         MISSING_SOURCE_GENES_EXPORT_WARNING: {
