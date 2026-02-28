b7ed786aefff6cb849ae76d77ea053c20e8192e7
dondi <dondi@lmu.edu>
2026-02-27 17:29:53 -0800

    #1296 Remove lingering extraneous backslash.

--- server/controllers/workbook-constants.js
+++ server/controllers/workbook-constants.js
@@ -648,8 +648,10 @@ module.exports = {
             return {
                 errorCode: "MISLABELED_NETWORK_CELL_A1",
                 possibleCause: `The top left cell of the ${sheetName} sheet is mislabeled.`,
-                suggestedFix: `Replace the incorrect label with '${CELL_A1_GRN}' for a gene regulatory network (GRN) or \
-                '${CELL_A1_PPI}' for a protein-protein interaction network (PPI) exactly.`,
+                suggestedFix: [
+                    `Replace the incorrect label with '${CELL_A1_GRN}' for a gene regulatory network (GRN)`,
+                    `or '${CELL_A1_PPI}' for a protein-protein interaction network (PPI) exactly.`,
+                ].join(" "),
             };
         },
     },
