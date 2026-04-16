912e4fb8341d1a6ab846d412b664c3799946764f
ceciliazaragoza <czarago3@lion.lmu.edu>
2026-04-16 07:57:53 -0700

    ignore web-client in web-client-classic eslint.config.js and update import for prettier config in web-client

--- web-client/eslint.config.js
+++ web-client/eslint.config.js
@@ -1,12 +1,8 @@
 import prettierPlugin from "eslint-plugin-prettier";
-// Using a longer way of importing prettier config to avoid linting issues from classic eslint
-import fs from "node:fs";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
+import { readFileSync } from "node:fs";
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const prettierConfig = JSON.parse(
-  fs.readFileSync(path.join(__dirname, ".prettierrc.json"), "utf8")
+  readFileSync(new URL("./.prettierrc.json", import.meta.url), "utf8")
 );
 
 export default [
@@ -14,7 +10,7 @@ export default [
     ignores: [
       "node_modules/**",
       "coverage/**",
-      "web-client-classic/public/js/*.min.js",
+      "web-client-classic/**",
       "coverage/lcov-report/**",
       "_site/**",
       ".git/**",
