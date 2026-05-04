a180c71357e3584e0adcab5735a20dcd56451dc4
Alex Miller <amill128@lion.lmu.edu>
2026-05-03 18:35:45 -0700

    Updated the warnings for issue #1561
    Updated the two-column sheet warnings when there are extra or missing genes in the special sheets described in issue #1561.

--- server/controllers/workbook-constants.js
+++ server/controllers/workbook-constants.js
@@ -444,15 +444,23 @@ module.exports = {
         },
 
         extraGenesWarning: function (sheetName, extraGenes) {
+            const prefix = sheetName.includes("optimized");
             return {
                 warningCode: `EXTRA_GENES_${sheetName.toUpperCase()}`,
-                errorDescription: [
-                    `GRNsight has detected that there are extra genes in the imported workbook's '${sheetName}' sheet.`,
-                    `The genes in the '${sheetName}' sheet need to match the genes in the 'network' sheet`,
-                    "to use this workbook` as an input file for GRNmap,",
-                    "but will not affect the display of the graph in GRNsight.",
-                    `The extra genes are: ${extraGenes}.`,
-                ].join(" "),
+                errorDescription: prefix
+                    ? [
+                          `GRNsight has detected that there are extra genes in the imported workbook's '${sheetName}' sheet.`,
+                          `The genes in the '${sheetName}' sheet should match the genes in the 'network' sheet`,
+                          `from the GRNmap output, but will not affect the display of the graph in GRNsight.`,
+                          `The extra genes are: ${extraGenes}.`,
+                      ].join(" ")
+                    : [
+                          `GRNsight has detected that there are extra genes in the imported workbook's '${sheetName}' sheet.`,
+                          `The genes in the '${sheetName}' sheet need to match the genes in the 'network' sheet`,
+                          "to use this workbook` as an input file for GRNmap,",
+                          "but will not affect the display of the graph in GRNsight.",
+                          `The extra genes are: ${extraGenes}.`,
+                      ].join(" "),
             };
         },
 
