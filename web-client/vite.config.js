8dfbb55ae7fec76e453df605bde0db19a8ca27ef
ceciliazaragoza <czarago3@lion.lmu.edu>
2026-05-05 13:03:19 -0700

    trying out automated deployment with __APP_VERSION__ in vite.config

--- web-client/vite.config.js
+++ web-client/vite.config.js
@@ -1,13 +1,19 @@
 /// <reference types="vitest/config" />
 import { defineConfig } from "vite";
+import { readFileSync } from "fs";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
+const pkgVersion =
+  process.env.npm_package_version ||
+  JSON.parse(readFileSync(new URL("./package.json", import.meta.url))).version;
+
 export default defineConfig({
   plugins: [react()],
   base: "/GRNsight/react-thesis-4081/",
   define: {
     __BUILD_DATE__: JSON.stringify(new Date().toISOString()),
+    __APP_VERSION__: JSON.stringify(pkgVersion),
   },
   test: {
     environment: "jsdom", // use this so that have access to document, window, etc. for testing components
