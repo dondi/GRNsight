24352f512e3101d5255e7cac47f9ba2bade9df05
ceciliazaragoza <czarago3@lion.lmu.edu>
2026-05-05 17:55:33 -0700

    access __APP_VERSION__ correctly and commit to gh-pages branch

--- web-client/src/components/Header.jsx
+++ web-client/src/components/Header.jsx
@@ -9,7 +9,7 @@ export default function Header() {
     <div id="header-container">
       <header>
         {`GRNsight React: `}
-        <span>{`v__APP_VERSION__} (${formattedBuildDate})`}</span>
+        <span>{`v${__APP_VERSION__} (${formattedBuildDate})`}</span>
       </header>
       <div id="disclaimer">
         <span>Disclaimer:</span>
