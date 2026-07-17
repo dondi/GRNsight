1d5330a74ebde54a2a0a2d5fcbe661e40baa7971
Alex Miller <amill128@lion.lmu.edu>
2026-07-17 13:19:02 -0700

    Updated logic of conditional
    Swapped the wording in the coditional to get rid of unnessary code

--- server/controllers/workbook-constants.js
+++ server/controllers/workbook-constants.js
@@ -416,16 +416,16 @@ module.exports = {
             const prefix = sheetName.includes("optimized");
             return {
                 warningCode: `MISSING_GENES_AND_VALUES_${sheetName.toUpperCase()}`,
-                errorDescription: !prefix
+                errorDescription: prefix
                     ? [
+                          `There were no genes and values supplied in the ${sheetName} sheet in the imported workbook`,
+                          grnMapInputSuppliedWarningMessage(sheetName, value),
+                      ].join(" ")
+                    : [
                           `GRNsight has detected that there are missing genes and ${valuesForEachTwoColSheet[sheetName]}`,
                           `in the imported workbook's ${sheetName} sheet.`,
                           grnMapInputSuppliedWarningMessage(sheetName, value),
                           `The missing genes and values are: ${missingGenes}.`,
-                      ].join(" ")
-                    : [
-                          `There were no genes and values supplied in the ${sheetName} sheet in the imported workbook`,
-                          grnMapInputSuppliedWarningMessage(sheetName, value),
                       ].join(" "),
             };
         },
