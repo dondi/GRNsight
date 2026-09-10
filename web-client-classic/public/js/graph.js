d56b283ef5bf0a18ac53c70693cee8ca1dbd185d
Cindy <cindyltong01@gmail.com>
2026-09-10 09:38:34 -0700

    Fix gene page symbol for PPI protein nodes

--- web-client-classic/public/js/graph.js
+++ web-client-classic/public/js/graph.js
@@ -1233,7 +1233,7 @@ export var drawGraph = function (workbook) {
                     href:
                         "info?" +
                         $.param({
-                            symbol: gene.name,
+                            symbol: grnState.mode === NETWORK_PPI_MODE ? gene.name.replace(/p$/i, "").toUpperCase() : gene.name,
                             species: grnState.genePageData.species,
                             jaspar: grnState.genePageData.taxonJaspar,
                             uniprot: grnState.genePageData.taxonUniprot,
