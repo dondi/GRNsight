c8a4b36bf77b3ebc2b156e130fe78c9b041466c5
Cindy <cindyltong01@gmail.com>
2026-09-10 09:48:03 -0700

    Fix linting for PPI gene symbol

--- web-client-classic/public/js/graph.js
+++ web-client-classic/public/js/graph.js
@@ -1233,7 +1233,9 @@ export var drawGraph = function (workbook) {
                     href:
                         "info?" +
                         $.param({
-                            symbol: grnState.mode === NETWORK_PPI_MODE ? gene.name.replace(/p$/i, "").toUpperCase() : gene.name,
+                            symbol: grnState.mode === NETWORK_PPI_MODE 
+                            ? gene.name.replace(/p$/i, "").toUpperCase()
+                            : gene.name,
                             species: grnState.genePageData.species,
                             jaspar: grnState.genePageData.taxonJaspar,
                             uniprot: grnState.genePageData.taxonUniprot,
